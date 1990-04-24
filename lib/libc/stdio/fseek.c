77483106772f2f41c733856baa481e808c90e646
System Administrator <root@localhost>
1990-04-23 21:31:51 -0500

    steve 4/23/90 Integrate MSDOS source changes into COHERENT libc source: adjust returned seek pointer for ungot character, clear FEOF.

--- lib/libc/stdio/fseek.c
+++ lib/libc/stdio/fseek.c
@@ -1,6 +1,6 @@
 /*
  * Standard I/O Library
- * Seek; first ensure buffer is clean; afterwards put ptrs at right place
+ * Seek; first ensure buffer is clean; afterwards put ptrs at right place.
  */
 
 #include <stdio.h>
@@ -13,11 +13,14 @@ int	origin;
 {
 	long	lseek();
 
+	if ((origin == SEEK_CUR) && (fp->_ff & _FUNGOT))
+		offset--;
 	if (_fpseek(fp)==EOF)
 		return (EOF);
 	if ((offset=lseek(fileno(fp), offset, origin)) == -1L)
 		return (EOF);
 	if (fp->_bp!=NULL)
 		fp->_dp = fp->_cp = fp->_bp + (unsigned)offset%BUFSIZ;
+	fp->_ff &= ~_FEOF;
 	return (0);
 }
