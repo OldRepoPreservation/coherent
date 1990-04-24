a3f8531ec5438d1cc471ab23811a4974fbe8138b
System Administrator <root@localhost>
1990-04-23 21:53:30 -0500

    steve 4/23/90 Integrated MSDOS stdio changes into COHERENT libc source. Use SEEK_CUR instead of explicit constant, adjust for ungot character.

--- lib/libc/stdio/ftell.c
+++ lib/libc/stdio/ftell.c
@@ -13,9 +13,11 @@ register FILE	*fp;
 	extern	long	lseek();
 	long	offset;
 
-	if ((offset=lseek(fileno(fp), 0L, 1)) == -1L)
+	if ((offset=lseek(fileno(fp), 0L, SEEK_CUR)) == -1L)
 		return (-1L);
-	else if (fp->_bp!=NULL)
+	if (fp->_bp!=NULL)
 		offset += fp->_cp - fp->_dp;
+	if (fp->_ff&_FUNGOT)
+		--offset;
 	return (offset);
 }
