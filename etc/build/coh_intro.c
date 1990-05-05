7a41981a0a70effadee3ae89cd9a8b496e342c84
System Administrator <root@localhost>
1990-05-04 23:17:41 -0500

    steve 5/4/90 Initial MWC RCS revision, corresponding to beta release object.

---
+++ etc/build/coh_intro.c
@@ -0,0 +1,198 @@
+/*
+ * coh_intro.c
+ * 4/19/90
+ * Usage: coh_intro
+ * Uses routines in build0.c: cc -o coh_intro coh_intro.c build0.c
+ */
+
+#include <stdio.h>
+#include "build0.h"
+
+#define	VERSION	"1.0"
+
+/* Forward. */
+void	tour();
+
+main(argc, argv) int argc; char *argv[];
+{
+	if (argc > 1 && argv[1][0] == '-' && argv[1][1] == 'V')
+		fprintf(stderr, "%s: V%s\n", argv[0], VERSION);
+	if (yes_no("Would you like an introductory tour of COHERENT"))
+		tour();
+	exit(0);
+}
+
+void
+tour()
+{
+	cls(0);
+	/* Startup and shutdown. */
+	printf(
+"When you boot your computer system (by turning on the power,\n"
+"by hitting the reset switch, or by typing <Ctrl><Alt><Del>),\n"
+"the bootstrap program by default runs the operating system on\n"
+"the partition marked as active.  To boot an operating system\n"
+"system on a different partition, type a partition number (0 to 7)\n"
+"when the boot tries to read the floppy disk.\n"
+"\n"
+"When you boot COHERENT, it starts up in single-user mode.\n"
+"It automatically executes the commands in the file /etc/brc,\n"
+"which typically check COHERENT filesystems for consistency\n"
+"using /etc/fsck.  If the filesystems check without errors,\n"
+"the system goes into multi-user mode, automatically executing\n"
+"the commands in the file /etc/rc.  You can interrupt file system\n"
+"checking by typing <Ctrl-C>.\n"
+"\n"
+"To shut down COHERENT from multi-user mode, log in as root and\n"
+"execute /etc/shutdown to return the system to single-user mode.\n"
+"Then type sync before rebooting or turning off the power.\n"
+"Your COHERENT filesystems may be damaged if you reboot or\n"
+"power down without following this procedure.\n"
+"\n"
+		);
+
+	/* Root. */
+	cls(1);
+	printf("Now we will take a quick tour of your COHERENT filesystem.\n");
+	printf("The root directory / contains:\n\n");
+	sys("lc /", S_IGNORE);
+	printf(
+"\n"
+"COHERENT executes file /.profile when the superuser root logs in.\n"
+"File /coherent (linked to /autoboot) is the COHERENT system image.\n"
+"Empty directories /f0 and /f1 are used for mounting diskettes.\n"
+"Empty directory /mnt is used for mounting other devices.\n"
+"The filesystem checking program /etc/fsck uses empty directory\n"
+"/lost+found if it finds a problem with the root filesystem.\n"
+"COHERENT uses directory /tmp for temporary files; it removes files\n"
+"in /tmp automatically when the system goes multiuser (via /etc/rc).\n"
+"\n"
+		);
+
+	/* /bin. */
+	cls(1);
+	printf(
+"Directory /bin contains executable programs (commands).\n"
+"Some of the most commonly used commands are:\n"
+"\tbc\tinteractive calculator with arbitrary precision\n"
+"\tcat\tconcatenate/print files\n"
+"\tcc\tC compiler\n"
+"\tcmp\tcompare files\n"
+"\tcp\tcopy files\n"
+"\tcpdir\tcopy directory hierarchy\n"
+"\tdiff\tsummarize differences between files\n"
+"\tdos\ttransfer files to or from MS-DOS\n"
+"\ted\tinteractive line-oriented text editor\n"
+"\tegrep\textended pattern search\n"
+"\thelp\tprint concise command description\n"
+"\tlc\tcategorize files in a directory\n"
+"\tls\tlist directory contents\n"
+"\tmail\tcomputer mail\n"
+"\tps\tprint process status\n"
+"\trm\tremove files\n"
+"\trmdir\tremove directories\n"
+"\tscat\tsegmented concatenation\n"
+"\tsort\tsort lines of text\n"
+"\twc\tcount words, lines, characters in a file\n"
+"The next screen lists all the commands in /bin.\n"
+		);
+	cls(1);
+	sys("lc /bin", S_IGNORE);
+
+	/* /conf. */
+	cls(1);
+	printf("Directory /conf contains:\n\n");
+	sys("lc /conf", S_IGNORE);
+	printf("\n");
+
+	/* /dev. */
+	cls(1);
+	printf("Directory /dev contains COHERENT devices:\n\n");
+	sys("lc /dev", S_IGNORE);
+	printf(
+"\n"
+"Some important devices are:\n"
+"\t/dev/at*\tCooked (block-by-block) hard disk partitions\n"
+"\t/dev/com*\tSerial port devices (COM1 and COM2)\n"
+"\t/dev/console\tConsole terminal\n"
+"\t/dev/f*\t\tFloppy disk devices\n"
+"\t/dev/null\tBit bucket\n"
+"\t/dev/rat*\tRaw (character-by-character) hard disk partitions\n"
+"\n"
+		);
+
+	/* /etc. */
+	cls(1);
+	printf("Directory /etc contains files and program used in system administration:\n\n");
+	sys("lc /etc", S_IGNORE);
+	printf(
+"\n"
+"Files of particular interest in /etc include:\n"
+"\t/etc/brc\tExecuted when the system comes up single-user\n"
+"\t/etc/group\tDefines user group membership information\n"
+"\t/etc/passwd\tDefines user login information\n"
+"\t/etc/profile\tExecuted for each shell\n"
+"\t/etc/rc\t\tExecuted when the system comes up multi-user\n"
+"\t/etc/ttys\tDefines status of terminals attached to system\n"
+"\n"
+		);
+
+	/* /lib. */
+	cls(1);
+	printf("Directory /lib contains libraries and C compiler phases:\n\n");
+	sys("lc /lib", S_IGNORE);
+	printf("\n");
+
+	/* /usr. */
+	cls(1);
+	printf("Directory /usr contains a number of subdirectories:\n\n");
+	sys("lc /usr", S_IGNORE);
+	printf(
+"\n"
+"\t/usr/adm\tSystem administration files\n"
+"\t/usr/bin\tAdditional COHERENT commands\n"
+"\t/usr/dict\tDictionary for spell command\n"
+"\t/usr/games\tGames\n"
+"\t/usr/include\tHeader files\n"
+"\t/usr/lib\tAdditional libraries\n"
+"\t/usr/man\tCOHERENT online manual pages\n"
+"\t/usr/msgs\tSystem-wide messages\n"
+"\t/usr/pub\tPublic information\n"
+"\t/usr/spool\tWork area for daemon processes\n"
+"\t/usr/src\tSome sources and sample programs\n"
+"\n"
+		);
+
+	/* Diskettes. */
+	cls(1);
+	printf(
+"To use a floppy disk with COHERENT, you must:\n"
+"\t(1) format it with /etc/fdformat,\n"
+"\t(2) build an empty filesystem on it with /etc/mkfs,\n"
+"\t(3) mount it with /bin/mount or /etc/mount,\n"
+"\t(4) copy files to or from it, e.g. with /bin/cp or /bin/cpdir,\n"
+"and\t(5) unmount it with /bin/umount or /etc/umount.\n"
+"\n"
+"Some commonly diskette device names and formats are:\n"
+"\tDevice name  Sectors/track  Heads  Sectors  Format\n"
+"\t/dev/f9a0          9          2      720    5.25\"\n"
+"\t/dev/fqa0          9          2     1440    3.5\"\n"
+"\t/dev/fha0         15          2     2400    5.25\"\n"
+"Device names ending in '0' and '1' indicate drives A: and B:.\n"
+"\n"
+"For example, to copy directory /dir to a 5.25\" high density diskette:\n"
+"\t/etc/fdformat /dev/fha0\n"
+"\t/etc/mkfs /dev/fha0 2400\n"
+"\t/etc/mount /dev/fha0 /f0\n"
+"\tcpdir -vd /dir /f0/dir\n"
+"\t/etc/umount /dev/fha0\n"
+"/bin/mount and /bin/umount provide handy abbreviations for mount commands.\n"
+"\n"
+		);
+
+	/* Done. */
+	cls(1);
+	printf("This concludes your brief introduction to COHERENT.\n");
+}
+
+/* end of coh_intro.c */
