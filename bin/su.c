f6f2a84ed66935d1be26877923bfcb28db4cc9f3
System Administrator <root@localhost>
1990-10-05 16:02:51 -0500

    steve 10/4/90 Fixed password bug; nonnull passwords for users other than root failed. Fixed gid bug, previously invisible because of preceding bug. Cleaned up code slightly.

--- bin/su.c
+++ bin/su.c
@@ -1,55 +1,56 @@
 /*
  * Rec'd from Lauren Weinstein, 7-16-84.
- * Substitute user-id temporarily
- * or become super user (as you wish).
- *
- * Compile -s -n -i
+ * Substitute user-id temporarily or become super user (as you wish).
+ * Compile -s -n -i.
+ * Hacked by steve 10/4/90 to correct password bug and for clarity.
  */
 
 #include <stdio.h>
 #include <pwd.h>
 
-extern char *getenv();
+extern	char *getenv();
+extern	char *getpass();
+extern	char *malloc();
 
 #define ACCNAME "remacc"	/* Remote access password dummy username */
+#define	DEFSHELL "/bin/sh"	/* Default shell pathname */
 #define PASSLEN 13		/* Encrypted password length */
 
-char	*getpass();
+#define	bye()	fatal("Sorry")
+
+/* Forward. */
+extern	void	addenviron();
+extern	void	fatal();
+extern	void	getuname();
+
+/* Globals. */
+char	*defargs[] = { "su", NULL };
+/* The following are all set by getuname(). */
 short	gid;
-char	*password;		/* Set by getuname */
+char	*password;
 char	salt[3];
-char	shell[] = "/bin/sh";
-char	*shargs[] = {
-	"su",
-	NULL
-};
+short	uid;
 
-char	prs[50] = "PS1=";
-char	*prompt = prs;
-
-main(argc, argv)
-char *argv[];
+main(argc, argv) int argc; char *argv[];
 {
-	register int uid;
-	register int count = 0;	
+	register int ouid, ogid;
 	char *command, *prompta, *promptb, *passp;
 	char **args;
 
-	if ((uid = getuname(argc>1 ? argv[1] : "0")) < 0) /* check username */
-	   bye();  /* yep */
-	if (password[0] != '\0' && getuid())   /* check password if not su */
-	{  passp = getpass("Password: ");  /* get input password choice */
-	   while (count++ < 2)
-	   {  if (count > 0)
-		 if (getuname("0") < 0)  /* check root password too */ 
-		    bye();  /* failure */
-	      if ((strlen(password) != PASSLEN) ||
-	            (strcmp(crypt(passp, salt), password))) 
-	      {  if (count > 0)  /* if we've tried both passwords */
-	            bye();  /* failure */
-	      }	
-	      else break;  /* password ok */
-	   }
+	getuname(argc>1 ? argv[1] : "0");	/* check username */
+	if (password[0] != '\0' && getuid()) {	/* check password if not already su */
+		passp = getpass("Password: ");	/* get input password choice */
+		if ((strlen(password) != PASSLEN)
+		 || (strcmp(crypt(passp, salt), password) != 0)) {
+			ouid = uid;
+			ogid = gid;
+			getuname("0");		/* check root password too */ 
+			if ((strlen(password) != PASSLEN)
+			 || (strcmp(crypt(passp, salt), password) != 0))
+				bye();		/* failure */
+			uid = ouid;
+			gid = ogid;
+		}
 	}
 
 	if (argc > 2) {
@@ -58,72 +59,40 @@ char *argv[];
 	} else {
 		command = getenv("SHELL");
 		if (command == NULL || strlen(command) < 1)
-			command = shell;
-		args = shargs;
+			command = DEFSHELL;
+		args = defargs;
 	}
 	setgid(gid);
 	setuid(uid);
-	prompta = getenv("PSN");  /* check for normal prompt */
-	promptb = getenv("PSS");  /* check for desired su prompt */
-	addenviron(uid == 0 ? (promptb ? promptb : "# ") :
-	   (prompta ? prompta : "$ "));  /* change prompt as appropriate */
+	prompta = getenv("PSN");		/* check for normal prompt */
+	promptb = getenv("PSS");		/* check for desired su prompt */
+	addenviron(uid == 0 ? (promptb ? promptb : "# ")
+			    : (prompta ? prompta : "$ "));
+						/* change prompt as appropriate */
 	execvp(command, args);
-	printf("%s: not found\n", command);
+	fatal("%s: not found", command);
 }
 
 /*
- * Get a user-name from a string.
- * If the string starts with a numeric use 
- * directly as a number.
- * The string `password' is set with
- * the user's password for checking later.
- * Returns uid.
+ * Add string 's' to the environment as "PS1".
  */
-getuname(s)
-register char *s;
-{
-	register struct passwd *pwp;
-	register short uid;
-
-	if (*s>='0' && *s<='9') {
-		uid = atoi(s);
-		if ((pwp = getpwuid(uid)) == NULL) {
-			fprintf(stderr, "%d: bad user number\n", uid);
-			exit(1);
-		}
-	} else if ((pwp = getpwnam(s)) == NULL) {
-		fprintf(stderr, "%s: not a user name\n", s);
-		exit(1);
-	}
-	if (!strcmp(pwp->pw_name, ACCNAME))  /* dummy access username? */
-	   return(-1);  /* yes */
-	password = pwp->pw_passwd;
-	salt[0] = pwp->pw_passwd[0];
-	salt[1] = pwp->pw_passwd[1];
-	salt[2] = '\0';
-	gid = pwp->pw_gid;
-	return (pwp->pw_uid);
-}
-
-/*
- * Add string `s' to the environment as "PS1".
- */
-addenviron(s)
-char *s;
+void
+addenviron(s) char *s;
 {
 	extern char **environ;
 	register char **epp1, **epp2;
 	register char **newenv;
 	int n;
-	char *malloc();
+	char *prompt;
+	static char prbuf[50];
 
 	for (epp1 = environ; *epp1!=NULL; epp1++)
 		;
 	n = (epp1-environ+2) * sizeof (char *);
-	if ((newenv = (char **)malloc(n)) == NULL) {
-		fprintf(stderr, "Out of memory for environments\n");
-		exit(1);
-	}
+	if ((newenv = (char **)malloc(n)) == NULL)
+		fatal("Out of memory for environments");
+	prompt = prbuf;
+	strcpy(prompt, "PS1=");
 	strcat(prompt, s);
 	for (epp1=environ, epp2=newenv; *epp1 != NULL; epp1++)
 		if (strncmp(*epp1, "PS1=", 4) != 0)
@@ -137,8 +106,38 @@ char *s;
 	environ = newenv;
 }
 
-bye()
-{	fprintf(stderr, "Sorry\n");
+void
+fatal(s) char *s;
+{
+	fprintf(stderr, "%r\n", &s);
 	exit(1);
 }
 
+/*
+ * Get a user-name from string 's'.
+ * If the string starts with a numeric, use it directly as a uid.
+ * Set globals password, salt[], gid and uid with the user's password info.
+ * Die if not found or illegal.
+ */
+void
+getuname(s) register char *s;
+{
+	register struct passwd *pwp;
+
+	if (*s >= '0' && *s <= '9') {
+		uid = atoi(s);
+		if ((pwp = getpwuid(uid)) == NULL)
+			fatal("%d: bad user number", uid);
+	} else if ((pwp = getpwnam(s)) == NULL)
+		fatal("%s: not a user name", s);
+	if (strcmp(pwp->pw_name, ACCNAME) == 0)	/* dummy access username? */
+		bye();				/* yes, sorry */
+	password = pwp->pw_passwd;
+	salt[0] = pwp->pw_passwd[0];
+	salt[1] = pwp->pw_passwd[1];
+	salt[2] = '\0';
+	gid = pwp->pw_gid;
+	uid = pwp->pw_uid;
+}
+
+/* end of su.c */
