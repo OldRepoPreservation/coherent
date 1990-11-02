750203cf6dcc19aba41f60f8d966e2bcf53bdebd
Steve Ness <steve@mwc.com>
1990-11-02 17:49:53 -0600

    steve 11/2/90 At norm's request, modified to build a single $PAGER command and then execute it, rather than executing multiple $PAGER commands for requests which print multiple manual sections.  The previous version did not work as desired on multiple manual sections with the usual $PAGER setting, because it did not wait for user input at the end of a manual section. This corresponds to the COHERENT V311 /bin/man executable.

--- bin/man.c
+++ bin/man.c
@@ -1,102 +1,110 @@
 /*
- * maninx.c
- * 10/15/90
+ * man.c
+ * 10/31/90
  * Usage: man [ -w ] [ topic ... ]
  * Quick and dirty man hack.
- * Reads manual index, scats manual section.
+ * Read manual index, print manual sections to screen via $PAGER.
  */
 
 #include <stdio.h>
+#include <string.h>
 
-#define	BBBIGBUF	32000			/* manual index buffer */
-#define	DEFPAGER	"exec /bin/scat -s"	/* default $PAGER */
-#define	MANHELP		"/usr/man/man.help"	/* manual help file */
-#define	MANINX		"/usr/man/man.index"	/* manual index */
-#define	NBUF		80			/* other buffers */
-#define	VERSION		"1.2"			/* version id */
+#define	BBBIGBUF	32000			/* index buffer size	*/
+#define	DEFPAGER	"exec /bin/scat -1"	/* default $PAGER	*/
+#define	MANHELP		"/usr/man/man.help"	/* manual help file	*/
+#define	MANINX		"/usr/man/man.index"	/* manual index	file	*/
+#define	NBUF		80			/* buf[] buffer size	*/
+#define	NCMD		512			/* cmd[] buffer size	*/
+#define	VERSION		"1.3"			/* version id		*/
 
 extern	char	*getenv();
-extern	char	*index();
 
 /* Globals. */
 char	buf[NBUF];
-char	cmd[NBUF];
+char	cmd[NCMD];
 char	*indexp;
 char	manindex[BBBIGBUF];
-char	*pager;
 int	wflag;
 
 /* Forward. */
+void	cmdcat();
 void	fatal();
 char	*lookup();
 void	nonfatal();
 
 main(argc, argv) int argc; char *argv[];
 {
-	int	i, fd, status;
-	char	*lp;
-	int	found;
+	int	i, fd, status, found, nfound;
+	char	*cp;
 
 	status = 0;
 
 	/* Look for environmental $PAGER. */
-	if ((pager = getenv("PAGER")) == NULL)
-		pager = DEFPAGER;
-		
+	if ((cp = getenv("PAGER")) == NULL)
+		cp = DEFPAGER;
+	strcpy(cmd, cp);
+
+	/* Parse option args. */		
 	if (argc > 1 && strcmp(argv[1], "-w") == 0) {
 		++wflag;
 		--argc;
 		++argv;
 	}
 	if (argc > 1 && strcmp(argv[1], "-V") == 0) {
-
 		fprintf(stderr, "man: V%s\n", VERSION);
 		--argc;
 		++argv;
 	}
+
+	/* If no args, print manual help information. */
 	if (argc == 1) {
-		/* No args: print manual help information. */
 		if (wflag)
 			printf("%s\n", MANHELP);
 		else {
-			sprintf(cmd, "%s %s", pager, MANHELP);
+			cmdcat(" ");
+			cmdcat(MANHELP);
 			system(cmd);
 		}
 		exit(status);
 	}
 
-	/* Args given, look up each.  First read the index. */
+	/* Args given.  First read the index. */
 	if ((fd = open(MANINX, 0)) == -1)
 		fatal("cannot open manual index %s", MANINX);
 	else if ((i = read(fd, manindex, BBBIGBUF)) == -1)
 		fatal("cannot read index buffer");
 	else if (i >= BBBIGBUF)
 		fatal("index file too large");
+	close(fd);
 #if	DEBUG
 	nonfatal("%s=%d bytes", MANHELP, i);
 #endif]
 
-	/* Process each arg. */
+	/* Look up each arg. */
+	nfound = 0;
 	for (i = 1; i < argc; i++) {
 #if	DEBUG
 		nonfatal("argv[%d]=%s", i, argv[i]);
 #endif
 		indexp = manindex;
 		found = 0;
+
 		/* Look up arg in index.  May find multiple hits. */
-		while ((lp = lookup(argv[i])) != NULL) {
+		while ((cp = lookup(argv[i])) != NULL) {
 			found++;
+			nfound++;
 #if	DEBUG
-			nonfatal("index line=%s", lp);
+			nonfatal("index line=%s", cp);
 #endif
 			if (wflag)
-				printf("/usr/man/%s\n", lp);
+				printf("/usr/man/%s\n", cp);
 			else {
-				sprintf(cmd, "%s '/usr/man/%s'", pager, lp);
+				cmdcat(" ");
+				cmdcat("/usr/man/");
+				cmdcat(cp);
 #if	DEBUG
 				nonfatal("command=%s", cmd);
 #endif
-				system(cmd);
 			}
 		}
 		if (found == 0) {
@@ -104,7 +112,26 @@ main(argc, argv) int argc; char *argv[];
 			status = 1;
 		}
 	}
+	if (!wflag && nfound)
+		system(cmd);
 	exit(status);
+}
+
+/*
+ * Concatenate given string to cmd buffer.
+ * Complain if too long.
+ */
+void
+cmdcat(s) register char *s;
+{
+	register int len;
+
+	len = strlen(cmd);
+	if (len + strlen(s) + 1 >= NCMD)
+{printf("cmd=%s\ns=%s\nlen=%d\n", cmd, s, len);
+		fatal("command buffer overflow");
+}
+	strcpy(&cmd[len], s);
 }
 
 /* Cry and die. */
@@ -124,18 +151,20 @@ lookup(s) char *s;
 {
 	register char *namep, *next;
 
-	while ((next = index(indexp, '\n')) != NULL) {
+	while ((next = strchr(indexp, '\n')) != NULL) {
+		if (next - indexp + 1 >= NBUF)
+			fatal("index buffer overflow");
 		strncpy(buf, indexp, next - indexp);
-		buf[next-indexp] = '\0';
+		buf[next-indexp] = '\0';	/* index line to buf */
 #if	DEBUG
 		nonfatal("[%s]", buf);
 #endif
-		namep = index(buf, ' ');
+		namep = strchr(buf, ' ');
 		indexp = next + 1;		/* bump to next index line */
 		if (namep == NULL)
 			continue;
 		else
-			*namep++ = '\0';
+			*namep++ = '\0';	/* NUL-terminate index entry */
 #if	DEBUG
 		nonfatal("buf=%s namep=%s", buf, namep);
 #endif
@@ -151,4 +180,4 @@ nonfatal(s) char *s;
 	fprintf(stderr, "man: %r\n", &s);
 }
 	
-/* end of maninx.c */
+/* end of man.c */
