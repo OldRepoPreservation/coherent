69be8c35c06ea48f2122b7eb3153061113e88ca1
System Administrator <root@localhost>
1990-12-15 01:04:36 -0600

    steve 12/14/90 Added second argument to print() for font downloading. Added font downloading prefix/postfix code. Improved handling of PCL commands followed by binary data. Added comments.

--- bin/lpr/hpprint.c
+++ bin/lpr/hpprint.c
@@ -1,12 +1,29 @@
+/*
+ * hpprint.c
+ * 12/13/90
+ * Print a file for the hp daemon.
+ * This is nontrivial for two reasons.
+ * First, the device might be a serial line,
+ * so this needs to worry about getting the mode bits right.
+ * Second, "hpr foo" should expand tabs in foo,
+ * but "hpr -f 1 font" should not muck with the binary font bits.
+ * This routine therefore knows some gory details about HP PCL.
+ */
+
 #include	<stdio.h>
 #include	<sgtty.h>
-extern int printing;
-extern FILE *lp;
 
-char escbuff[64];
+extern	int	printing;
+extern	FILE	*lp;
 
-print( file)
-char	*file;
+char ebuf[64];
+
+/*
+ * Print the given file.
+ * If fontnum is not -1, the file contains a soft font;
+ * precede and follow it with PCL commands to load it as font fontnum.
+ */
+print(file, fontnum) char *file; int fontnum;
 {
 	register FILE	*f;
 	register int c;
@@ -22,6 +39,8 @@ char	*file;
 	sg.sg_flags &= ~(XTABS|CRMOD);
 	ioctl(fileno(lp), TIOCSETP, &sg);
 	sg.sg_flags = c;
+	if (fontnum != -1)
+		fprintf(lp, "\033*c%dD", fontnum);	/* assign font # */
 	while ((c=getc(f)) != EOF && printing > 0) {
 		if (nraw) {
 			putc(c, lp);
@@ -30,18 +49,13 @@ char	*file;
 		}
 		switch (c) {
 		case 033:
-			switch (escape(f)) {
-			case 'E':
-				continue;
-			case 'W':
-				if (escbuff[1] == '*' && escbuff[2] == 'b')
-					nraw = atoi(escbuff+3);
-				continue;
-			case 'X':
-				if (escbuff[1] == '&' && escbuff[2] == 'p')
-					nraw = atoi(escbuff+3);
-				continue;
-			}
+			c = escape(f);
+			/* PCL commands followed by binary data. */
+			if ((c == 'W' && ebuf[1] == '*' && ebuf[2] == 'b')
+			 || (c == 'W' && ebuf[1] == '(' && ebuf[2] == 's')
+			 || (c == 'W' && ebuf[1] == ')' && ebuf[2] == 's')
+			 || (c == 'X' && ebuf[1] == '&' && ebuf[2] == 'p'))
+				nraw = atoi(ebuf+3);		/* data count */
 			continue;
 		case '\n':
 			putc('\r', lp);
@@ -67,31 +81,40 @@ char	*file;
 			continue;
 		}
 	}
+	if (fontnum != -1)
+		fprintf(lp, "\033*c5F");	/* make font permanent */
 	ioctl(fileno(lp), TIOCSETP, &sg);
 	fclose( f);
 	return (0);
 }
-/* Buffer up escape sequences */
+
+/*
+ * Copy a PCL command (escape sequence) to the printer,
+ * but save a copy in the buffer for later examination.
+ */
 escape(fp) register FILE *fp;
 {
 	register char *p;
 	register int c;
-	p = escbuff;
+
+	p = ebuf;
 	*p++ = 033;
 	putc(033, lp);
 	if ((c = getc(fp)) != EOF && printing > 0) {
 		putc(c, lp);
 		*p++ = c;
 		*p = 0;
-		if (c == '=' || c == '9' || c == 'Y' || c == 'Z')
+		if (c == 'E' || c == '=' || c == '9' || c == 'Y' || c == 'Z')
 			return c;
 		while ((c = getc(fp)) != EOF && printing > 0) {
 			putc(c, lp);
 			*p++ = c;
 			*p = 0;
 			if (c >= '@' && c <= 'Z')
-				return c;
+				return c;	/* read until UPPERCASE char */
 		}
 	}
 	return 0;
 }
+
+/* end of hpprint.c */
