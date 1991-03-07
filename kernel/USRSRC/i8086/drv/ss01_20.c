990e13a7979356cee773d25291e3de0d018dc24a
System Administrator <root@localhost>
1991-03-07 16:41:31 -0600

    sends Test Ready, Starts to Request Sense

--- kernel/USRSRC/i8086/drv/ss01_20.c
+++ kernel/USRSRC/i8086/drv/ss01_20.c
@@ -2,14 +2,17 @@
  * This is a driver for Seagate ST01/ST02 scsi hard disk controllers.
  *
  * $Log:	/usr/src/sys/i8086/drv/RCS/ss.c,v $
+ * Revision 1.5	91/03/07  11:48:39	root
+ * Now sends Identify and Abort messages & completes a SCSI bus cycle
+ *
  * Revision 1.4	91/03/06  16:31:45	root
  * tried to send Identify message - get status 0x40 & fail
- * 
+ *
  * Revision 1.3	91/03/05  17:03:43	root
  * Goes thru arbitration (sans IRQ) successfully
- * 
+ *
  */
- 
+
 /*
  * Definitions.
  */
@@ -29,7 +32,7 @@
 #define WC_BUSY  	0x04
 #define WC_SELECT  	0x02
 #define WC_SCSI_RESET  	0x01
- 
+
 #define RS_ARBIT_COMPL	0x80	/* Read STATUS (RS) register bits */
 #define RS_PRTY_ERROR	0x40
 #define RS_SELECT	0x20
@@ -40,21 +43,34 @@
 #define RS_BUSY  	0x01
 
 #define HOST_ID		0x80	/* Host adapter is SCSI ID #7 */
-#define BUS_RETRIES	10000
+#define BUS_RETRIES	1000
+
+				/* Message types */
 #define MSG_IDENT_DC	0xC0	/* Identify, with Disconnect allowed */
-#define MSG_ABORT	0x06	/* Identify, with Disconnect allowed */
- 
+#define MSG_ABORT	0x06	/* End the current SCSI bus cycle */
+
+#define CS_GOOD		0x00	/* Command Status from the drive */
+#define CS_CHECK	0x02
+#define CS_BUSY		0x08
+#define CS_RESERVED	0x18
+
 #if 1
+int stats[40], statsptr;
+#define PUSHI		{ stats[statsptr++] = i; }
+#define POPI		{ printf("%d:",statsptr);while(statsptr)\
+				printf("%d ",stats[--statsptr]);printf("\n");}
 #define SSTELL(foo)	printf(foo)
 #define SSTATUS		printf("status=%x\n", (int)(unsigned char)status)
 #else
+#define PUSHI
+#define POPI
 #define SSTELL(foo)
 #define SSTATUS
 #endif
 
 /*
  * Includes.
- */ 
+ */
 #include	<coherent.h>
 #include	<sys/io.h>
 #include	<sys/sched.h>
@@ -73,7 +89,7 @@
 /*
  * Export Functions.
  */
- 
+
 /*
  * Export Variables - patch these to configure the driver.
  */
@@ -145,12 +161,15 @@ ssload()
 	int i;
 	char status;
 	int await_bus;
+	int data1, data2;
+#define CMDLEN 6
+	unsigned char cmd[CMDLEN];
 
 	/*
 	 * Claim IRQ vector.
 	 */
-	setivec(SS_INT, ssintr); 
-	
+	setivec(SS_INT, ssintr);
+
 	/*
 	 * Allocate a selector to map into ST0x memory-mapped comm area.
 	 */
@@ -176,7 +195,7 @@ ssload()
 		return;
 	} else
 		printf("ST0x passed memory test\n");
-		
+
 #if 1
 { long x;
 	/*
@@ -186,105 +205,126 @@ ssload()
 	for (x=0; x<100000L; x++);
 	sfbyte(ss_csr, 0);
 	for (x=0; x<100000L; x++);
-}	
+}
 #endif
-	 
+
 	/*
 	 * Do ST0x arbitration.
-	 */	
+	 */
 	sfbyte(ss_csr, 0);		/* De-assert SCSI enable bit */
 	sfbyte(ss_dat, HOST_ID);	/* Write my SCSI id to port */
 	sfbyte(ss_csr, WC_ARBITRATE);	/* Start arbitration */
 
-	for ( i = 0, await_bus = 1; await_bus && i < BUS_RETRIES; i++) {
-		status = ffbyte(ss_csr);
-		if (status & RS_ARBIT_COMPL) {
-			await_bus = 0;
-		}
-	}
-	if (await_bus) {
-		printf("Error - ST0x doesn't complete arbitration\n");
-		return;
-	}
+	bus_wait(RS_ARBIT_COMPL << 8 | RS_ARBIT_COMPL);
 
 	/*
 	 * Arbitration complete.  Now select, with ATN to allow messages.
 	 */
 	sfbyte(ss_dat, HOST_ID | 1);	/* Write two SCSI id's to port */
 	sfbyte(ss_csr, WC_ENABLE_SCSI | WC_ATTENTION | WC_SELECT);
-	
-	for ( i = 0, await_bus = 1; await_bus && i < BUS_RETRIES; i++) {
-		status = ffbyte(ss_csr);
-		if (status & RS_BUSY) {
-			await_bus = 0;
-		}
-	}
-	if (await_bus) {
-		printf("Error - ST0x drive doesn't assert BUSY\n");
-		return;
-	}
+
+	bus_wait(RS_BUSY << 8 | RS_BUSY);
 
 	/*
 	 * Send "Identify" Message with Disconnect allowed.
-	 */	
+	 */
 	sfbyte(ss_csr, WC_ENABLE_SCSI | WC_ATTENTION);
-	for ( i = 0, await_bus = 1; await_bus && i < BUS_RETRIES; i++) {
-		status = ffbyte(ss_csr);
-		if ((status & (RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE)) ==
-			(RS_REQUEST|RS_CTRL_DATA|RS_MESSAGE)) {
-			await_bus = 0;
-		}
-	}
-	if (await_bus) {
-		printf("Error - ST0x didn't enter MSG out\n");
-		return;
-	}
 
+	bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+		(RS_REQUEST|RS_CTRL_DATA|RS_MESSAGE));
+
+#ifdef SEND_ABORT_MESSAGE
 	sfbyte(ss_csr, WC_ENABLE_SCSI | WC_ATTENTION);
 	sfbyte(ss_dat, MSG_IDENT_DC);
-	
+
 	/*
 	 * Second message is Abort.
 	 */
-	for ( i = 0, await_bus = 1; await_bus && i < BUS_RETRIES; i++) {
-		status = ffbyte(ss_csr);
-		if ((status & (RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE)) ==
-			(RS_REQUEST|RS_CTRL_DATA|RS_MESSAGE)) {
-			await_bus = 0;
-		}
-	}
-	if (await_bus) {
-		printf("Error - ST0x didn't enter MSG out 2nd time\n");
-		return;
-	}
+	bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+		(RS_REQUEST|RS_CTRL_DATA|RS_MESSAGE));
 	sfbyte(ss_csr, WC_ENABLE_SCSI);
 	sfbyte(ss_dat, MSG_ABORT);
 
-	for ( i = 0, await_bus = 1; await_bus && i < BUS_RETRIES; i++) {
-		status = ffbyte(ss_csr);
-		if ((status & RS_BUSY) == 0) {
-			await_bus = 0;
-		}
+	bus_wait(RS_BUSY << 8 | RS_BUSY);
+#else
+	sfbyte(ss_csr, WC_ENABLE_SCSI);
+	sfbyte(ss_dat, MSG_IDENT_DC);
+
+for (i = 0; i < CMDLEN; i++) cmd[i] = 0; /* send Test Unit Ready command */
+
+	for (i = 0; i < CMDLEN; i++) {
+		bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+			(RS_REQUEST|RS_CTRL_DATA));
+		sfbyte(ss_dat, cmd[i]);
 	}
-	if (await_bus) {
-		printf("Error - ST0x didn't go to Bus Free state\n");
-		return;
-	}
-#if 0
-	for ( i = 0, await_bus = 1; await_bus && i < BUS_RETRIES; i++) {
-		status = ffbyte(ss_csr);
-		if (status & RS_REQUEST) {
-			await_bus = 0;
-		}
-	}
-	if (await_bus) {
-		printf("Error - ST0x didn't REQ after Identify msg\n");
-		return;
-	}
-#endif	
+#endif
+
+	bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+		(RS_REQUEST|RS_CTRL_DATA|RS_I_O));
+	data1 = ffbyte(ss_dat);
+	bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+		(RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE));
+	data2 = ffbyte(ss_dat);
+bus_wait(RS_BUSY, 0);
+status = ffbyte(ss_csr);
 SSTATUS;
-printf("i=%d\n", i);
-	
+printf("data1=%x data2=%x\n",data1,data2);
+POPI;
+	/*
+	 * If there was a check condition on the previous commmand,
+	 * do a Request Sense command.
+	 */
+	if (data1 & CS_CHECK) {
+		sfbyte(ss_csr, 0);		/* De-assert SCSI enable bit */
+		sfbyte(ss_dat, HOST_ID);	/* Write my SCSI id to port */
+		sfbyte(ss_csr, WC_ARBITRATE);	/* Start arbitration */
+
+		bus_wait(RS_ARBIT_COMPL << 8 | RS_ARBIT_COMPL);
+
+		/*
+		 * Arbitration complete.  Now select, with ATN to allow messages.
+		 */
+		sfbyte(ss_dat, HOST_ID | 1);	/* Write two SCSI id's to port */
+		sfbyte(ss_csr, WC_ENABLE_SCSI | WC_ATTENTION | WC_SELECT);
+
+		bus_wait(RS_BUSY << 8 | RS_BUSY);
+
+		/*
+		 * Send "Identify" Message with Disconnect allowed.
+		 */
+		sfbyte(ss_csr, WC_ENABLE_SCSI | WC_ATTENTION);
+
+		bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+			(RS_REQUEST|RS_CTRL_DATA|RS_MESSAGE));
+
+		sfbyte(ss_csr, WC_ENABLE_SCSI);
+		sfbyte(ss_dat, MSG_IDENT_DC);
+
+for (i = 0; i < CMDLEN; i++) cmd[i] = 0; /* send Test Unit Ready command */
+cmd[0] = 3;
+
+		for (i = 0; i < CMDLEN; i++) {
+			bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+				(RS_REQUEST|RS_CTRL_DATA));
+			sfbyte(ss_dat, cmd[i]);
+		}
+
+		bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+			(RS_REQUEST|RS_CTRL_DATA|RS_I_O));
+		data1 = ffbyte(ss_dat);
+		bus_wait(((RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE) << 8) |
+			(RS_REQUEST|RS_CTRL_DATA|RS_I_O|RS_MESSAGE));
+		data2 = ffbyte(ss_dat);
+bus_wait(RS_BUSY, 0);
+status = ffbyte(ss_csr);
+SSTATUS;
+printf("data1=%x data2=%x\n",data1,data2);
+POPI;
+	}
+
+#if 0
+bus_wait(RS_REQUEST << 8 | RS_REQUEST);
+#endif
 	/*
 	 * Initialize Drive Size.
 	 */
@@ -304,12 +344,12 @@ ssunload()
 	/*
 	 * Release IRQ vector.
 	 */
-	clrivec(SS_INT); 
-	 
+	clrivec(SS_INT);
+
 	/*
 	 * Free the ST0x selector.
 	 */
-	vrelse(ss_fp); 
+	vrelse(ss_fp);
 }
 
 /**
@@ -434,4 +474,36 @@ ssintr()
 
 static void	sswatch()
 {
+}
+
+/*
+ * Wait for specified bit values to appear in Status Register.
+ * This uses a tight loop and does not expect to be interrupted.
+ *
+ * Argument "flags" is a double-byte value;  the high byte is ANDed with
+ * status register contents, and the result is tested for equality with
+ * the low byte.
+ *
+ * Return 1 if values wanted appeared, 0 if timeout occurred.
+ */
+static int bus_wait(flags)
+unsigned short flags;
+{
+	int found, i;
+	unsigned char status;
+
+	found = 0;
+	for ( i = 0; i < BUS_RETRIES; i++) {
+		status = ffbyte(ss_csr);
+		if ((status & (flags >> 8)) == (flags & 0xff)) {
+			found = 1;
+			break;
+		}
+	}
+
+	if (!found)
+		printf("ST0x timeout;  flags=%x status=%x\n", flags, status);
+
+PUSHI;
+	return found;
 }
