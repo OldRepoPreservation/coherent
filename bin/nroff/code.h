ba9c4b18ba7e74a1a99ee039a2d9e6f02afeaf2b
System Administrator <root@localhost>
1991-03-19 20:02:46 -0600

    update prov by stevesf

--- bin/nroff/code.h
+++ bin/nroff/code.h
@@ -1,4 +1,5 @@
 /*
+ * code.h
  * Nroff/Troff.
  * Data structure for storing character codes and stream directives.
  */
@@ -6,19 +7,17 @@
 /*
  * Stream directives.
  */
-#define DNULL	0			/* Null */
-#define DHMOV	-1			/* Move horizontally */
-#define DVMOV	-2			/* Move vertically */
-#define DFONT	-3			/* Change font */
-#define DPSZE	-4			/* Change pointsize */
-#define DSPAR	-5			/* Space down and return */
-#define DPADC	-6			/* Paddable character */
-#define DHYPH	-7			/* Place to hyphenate */
-#define	DHYPC	(-8)			/* Hyphen character */
-#define	DTRAN	(-9)			/* Transparent character (dag)*/
-#define	DTRAB	(-10)			/* Transparent buffer 	 (dag)*/
-#define	DSPEC	(-11)			/* Special character	 (dag)*/
-#define	DFPOS	(-12)			/* Fix position		 (dag)*/
+#define DNULL	0			/* Null				*/
+#define DHMOV	(-1)			/* Move horizontally		*/
+#define DVMOV	(-2)			/* Move vertically		*/
+#define DFONT	(-3)			/* Change font			*/
+#define DPSZE	(-4)			/* Change pointsize		*/
+#define DSPAR	(-5)			/* Space down and return	*/
+#define DPADC	(-6)			/* Paddable character		*/
+#define DHYPH	(-7)			/* Place to hyphenate		*/
+#define	DHYPC	(-8)			/* Hyphen character		*/
+#define	DTRAB	(-9)			/* Transparent buffer 	 (dag)	*/
+#define	DFPOS	(-10)			/* Fix position		 (dag)	*/
 
 /*
  * All characters and commands are stored in this structure.
@@ -27,17 +26,17 @@
  * element is a character code and 0 if the element is a command.
  */
 typedef	union	{
-	struct	{			/* Structure containing character */
+	struct c_arg {			/* Structure containing character */
 		int	c_code;		/* Character code */
 		unsigned c_move;	/* Distance to move after char */
 		int	c_char;		/* If special character, char */
 	} c_arg;
-	struct	{			/* Command with one long argument */
+	struct l_arg {			/* Command with one long argument */
 		int	c_code;		/* Type of command */
 		int	c_iarg;		/* Long parameter */
 		int	c_csp;		/* Pre tab space */
 	} l_arg;
-	struct	{			/* Command with buffer ptr. (dag)*/
+	struct b_arg {			/* Command with buffer ptr. (dag)*/
 		int	c_code;		/* Type of command		*/
 		char	*c_bufp;	/* Pointer to buffer		*/
 	} b_arg;
@@ -51,43 +50,8 @@ typedef	union	{
 #define ifcdirc(c)	((c)<0)
 
 /*
- * Add a character given the font number and width.
- */
-#define addchar(f, w) {							\
-	chkcode();							\
-	nlinptr->c_code = f;						\
-	nlinptr->c_csp = 0;						\
-	nlinptr++->c_move = w;						\
-}
-
-/*
- * Add a directive which takes an integer argument.
- */
-#define addidir(d, i) {							\
-	chkcode();							\
-	nlinptr->c_code = d;						\
-	nlinptr++->c_iarg = i;						\
-}
-
-/*
- * Add a transparent buffer directive
- */
-#define	addtrab(bp) {							\
-	chkcode();							\
-	nlinptr->c_code = DTRAB;					\
-	nlinptr++->c_bufp = (bp);					\
-}
-
-/*
- * Make sure there is space to add another command.
- */
-#define chkcode() {							\
-	if (nlinptr >= &linebuf[LINSIZE])				\
-		panic(lbomsg);						\
-}
-
-/*
  * Global variables.
  */
 extern	CODE	codeval;		/* Diversion struct returned by getl */
-extern	char	lbomsg[];		/* "Line buffer overflow" */
+
+/* end of code.h */
