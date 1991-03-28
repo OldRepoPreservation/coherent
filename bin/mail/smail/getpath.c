bfa9f2403cad7f552ca084a7afc51b87f0ed6cdb
La Monte Yarroll <piggy@mwc.com>
1991-03-28 13:30:41 -0600

    Changed from binary to linear search. There was a bug in the binary search so that it failed for one line paths files.

--- bin/mail/smail/getpath.c
+++ bin/mail/smail/getpath.c
@@ -21,41 +21,18 @@ char *key;		/* what we are looking for */
 char *path;		/* where the path results go */
 int *cost;		/* where the cost results go */
 {
-	long pos, middle, hi, lo;
-	static long pathlength = 0;
 	register char *s;
 	int c;
 	static FILE *file;
-	int flag;
 
 DEBUG("getpath: looking for '%s'\n", key);
 
-			(void) printf("can't access %s.\n", pathfile);
-			pathlength = -1;
-		} else {
-			(void) fseek(file, 0L, 2);	/* find length */
-			pathlength = ftell(file);
-		}
+	if((file = fopen(pathfile, "r")) == NULL) {
 		(void) printf("can't access %s.\n", pathfile);
 		return( EX_OSFILE );
 	}
-	if( pathlength == -1 )
-		return( EX_OSFILE );
 
-*/
-	for( ;; ) {
-		pos = middle = ( hi+lo+1 )/2;
-		(void) fseek(file, pos, 0);	/* find midpoint */
-		if(pos != 0)
-			while(((c = getc(file)) != EOF) && (c != '\n'))
-				;	/* go to beginning of next line */
-		if(c == EOF) {
-			return(EX_NOHOST);
-		}
-		for( flag = 0, s = path; flag == 0; s++ ) { /* match??? */
-			if( *s == '\0' ) {
-				goto solved;
-			}
+	/* Linear search for key "path" in file stream "file".  */
 	fseek(file, 0L, 0); /* Rewind the file pointer "file".  */
 
 	for (c = getc(file); c != EOF; c = getc(file)) {
@@ -64,8 +41,16 @@ DEBUG("getpath: looking for '%s'\n", key);
 			/* NB: lower is a macro which evals its arg twice!  */
 			s++;
 			if((c = getc(file)) == EOF) {
+				fclose(file);
 				return(EX_NOHOST);
 			}
+		} /* while (lower(c) == lower(*s++)) */
+
+		if (*s == '\0') {
+			if ((c == '\t') || (c == ' ')){
+				break;
+			} /* if found seperator character */
+		} /* if key hit end of string */
 
 		while ((c != '\n') && (c != EOF)){
 			c = getc(file);	
@@ -77,10 +62,7 @@ DEBUG("getpath: looking for '%s'\n", key);
 		fclose(file);
 		return(EX_NOHOST);
 	}
-** Now just copy the result.
-*/
-solved:
-	while(((c  = getc(file)) != EOF) && (c != '\t') && (c != '\n')) {
+
 	while(((c = getc(file)) != EOF) && (c != '\t') && (c != '\n')) {
 		*path++ = c;
 	}
@@ -98,5 +80,6 @@ solved:
 		}
 		if(tcost >= 0) *cost = tcost;
 	}
+	fclose(file);
 	return (EX_OK);
 }
