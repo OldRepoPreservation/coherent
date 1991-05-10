84208ea9607b00924218a0f5b1bc066dc8d836cd
System Administrator <root@localhost>
1991-05-09 20:24:24 -0500

    added an 'unbuffered' mode

--- bin/tee.c
+++ bin/tee.c
@@ -1,95 +1,109 @@
-
-
 /*
+ * tee.c
+ * 4/30/91
+ * Pipe redirection.
+ * Usage: tee [ -a ] [ -i ] [ file ] ...
  * Rec'd from Lauren Weinstein, 7-16-84.
- * tee -- pipe redirection
- *	hacked by rec to use stdio so that everything doesn't come
- *	out buffered.
+ * Hacked by rec to use stdio so that everything doesn't come out buffered.
+ * Slightly rewritten by steve 4/30/91 for clarity; added "isatty" check.
  */
-#include	<signal.h>
-#include	<errno.h>
+
 #include	<stdio.h>
+#include	<errno.h>
+#include	<signal.h>
 
+#define	NUFILE	_NFILE			/* max # open files */
+#define	USAGE	"Usage: tee [ -a ] [ -i ] [ file ] ...\n"
 
-#define	NUFILE	20			/* max # open files */
+/* External. */
+extern	int	exit();
 
-
-int	aflag;				/* append to output files */
-char	iobuf[BUFSIZ];
+/* Forward. */
 FILE	*openf();
+void	fatal();
 
-
-main( argc, argv)
-register char	**argv;
+main(argc, argv) int argc; register char *argv[];
 {
-	register int	c;
-	register FILE	**fpp;
-	FILE	*fp[NUFILE];
-	extern int exit();
+	register int c;
+	register FILE **fpp;
+	FILE *fp[NUFILE];
+	int aflag;
 
 	if (signal(SIGINT, SIG_IGN) != SIG_IGN)
 		signal(SIGINT, exit);
-	while (*++argv && argv[0][0]=='-')
+
+	/* Process option arguments. */
+	aflag = 0;
+	while (*++argv && argv[0][0]=='-') {
 		switch (argv[0][1]) {
-		case 'i':
-			signal( SIGINT, SIG_IGN);
-			break;
 		case 'a':
 			++aflag;
 			break;
+		case 'i':
+			signal(SIGINT, SIG_IGN);
+			break;
 		default:
-			fprintf(stderr,
-			   "Usage: tee [ -a ] [ -i ] [ file ] ...\n");
+			fprintf(stderr, USAGE);
 			exit(1);
-			}
+		}
+	}
 
-	for (fpp=fp; *argv; ) {
+	/* Open file arguments. */
+	for (fpp = fp; *argv; ) {
 		if (fpp >= &fp[NUFILE])
-			fatal( "too many files");
-		*fpp++ = openf( *argv++);
+			fatal("too many files");
+		*fpp++ = openf(*argv++, aflag);
 	}
 	*fpp = NULL;
 
+	/* Make i/o unbuffered if interactive. */
+	if (isatty(fileno(stdin)))
+		setbuf(stdout, NULL);
+	if (isatty(fileno(stdout)))
+		setbuf(stdin, NULL);
+
+	/* Copy stdin to stdout, duplicate to each specified file. */
 	while ((c = getchar()) != EOF) {
 		putchar(c);
-		for (fpp=fp; *fpp!=NULL; fpp++)
+		for (fpp = fp; *fpp != NULL; fpp++)
 			putc(c, *fpp);
 	}
 
-	return (0);
+	/* Done. */
+	exit(0);
 }
 
+/*
+ * Cry and die.
+ */
+/* VARARGS */
+void
+fatal(s) char *s;
+{	fprintf(stderr, "tee: %r\n", &s);
+	exit(1);
+}
 
+/*
+ * Open a file.
+ */
 FILE *
-openf( file)
-char	*file;
+openf(file, aflag) char *file; int aflag;
 {
-	register FILE	*fp;
-	extern		errno;
+	register FILE *fp;
 
-	if (aflag) {
-		fp = fopen( file, "a");
-		if (fp != NULL) {
-			fseek(fp, 0L, 2);
-			return (fp);
-		}
-	} else {
-		fp = fopen( file, "w");
-		if (fp != NULL)
-			return (fp);
+	if ((fp = fopen(file, (aflag) ? "a" : "w")) != NULL) {
+		if (aflag)
+			fseek(fp, 0L, SEEK_END);
+		return fp;
 	}
 	switch (errno) {
 	case EMFILE:
 	case ENFILE:
-		fatal( "too many files");
+		fatal("too many files");
 		break;
 	default:
-		fatal( "can't create %s", file);
+		fatal("cannot create %s", file);
 	}
 }
 
-
-fatal( arg0)
-{	fprintf( stderr, "tee: %r\n", &arg0);
-	exit( 1);
-}
+/* end of tee.c */
