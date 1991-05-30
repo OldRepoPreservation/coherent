63eb678efbb9ba6ea32f6d7df41991790c902bd7
System Administrator <root@localhost>
1991-05-30 00:06:23 -0500

    steve 5/29/91 Added changes from hal. Nonsubstantive changes to whitespace.

--- lib/libc/sys/i8086/msgop.c
+++ lib/libc/sys/i8086/msgop.c
@@ -1,11 +1,9 @@
 /*
+ * libc/sys/msgop.c
  * User Message Functions.
- *
- *	Note: msgget() must be first function called.
- *
- *	91/02/07	Hal Snyder	mwchwc!/u/libc/sys/msgop.c
- *	msgget():  sizeof(key_t) is 4, not 2.
+ * Note: msgget() must be first function called.
  */
+ 
 #include <sys/msg.h>
 #include <errno.h>
 
@@ -15,17 +13,11 @@ static char msgdev[] = "/dev/msg";
 /*
  * Message Control Operations.
  */
-
-msgctl( msqid, cmd, buf )
-
-int msqid;
-int cmd;
-struct msqid_ds * buf;
-
+msgctl(msqid, cmd, buf) int msqid, cmd; struct msqid_ds * buf;
 {
 	int parm[4];
 
-	if ( msgfno < 0 ) {
+	if (msgfno < 0) {
 		errno = ENODEV;
 		return -1;
 	}
@@ -35,27 +27,19 @@ struct msqid_ds * buf;
 	parm[2] = cmd;
 	parm[3] = (int) buf;
 
-	ioctl( msgfno, MSGCTL, parm );
+	ioctl(msgfno, MSGCTL, parm);
 	return parm[0];
 }
 
 /*
  * Get Message Queue.
  */
-
-msgget( key, msgflg )
-
-key_t key;
-int msgflg;
-
+msgget(key, msgflg) key_t key; int msgflg;
 {
-	int parm[4];
+	int parm[3];
 
-	if ( msgfno < 0 ) {
-
-		msgfno = open(msgdev, 0);
-
-		if ( msgfno < 0 ) {
+	if (msgfno < 0) {
+		if ((msgfno = open(msgdev, 0)) < 0) {
 			perror(msgdev);
 			errno = ENODEV;
 			return -1;
@@ -64,28 +48,21 @@ int msgflg;
 
 	parm[0] = -1;
 	parm[1] = key;
-	parm[2] = key >> 16;
-	parm[3] = msgflg;
+	parm[2] = msgflg;
 
-	ioctl( msgfno, MSGGET, parm );
+	ioctl(msgfno, MSGGET, parm);
 	return parm[0];
 }
 
 /*
  * Send Message.
  */
- 
-msgsnd( msqid, msgp, msgsz, msgflg )
-
-int msqid;
-struct msgbuf *msgp;
-int msgsz;
-int msgflg;
-
+msgsnd(msqid, msgp, msgsz, msgflg)
+int msqid; struct msgbuf *msgp; int msgsz, msgflg;
 {
 	int parm[5];
 
-	if ( msgfno < 0 ) {
+	if (msgfno < 0) {
 		errno = ENODEV;
 		return -1;
 	}
@@ -96,26 +73,19 @@ int msgflg;
 	parm[3] = msgsz;
 	parm[4] = msgflg;
 
-	ioctl( msgfno, MSGSND, parm );
+	ioctl(msgfno, MSGSND, parm);
 	return parm[0];
 }
 
 /*
  * Receive Message.
  */
- 
-msgrcv( msqid, msgp, msgsz, msgtyp, msgflg )
-
-int msqid;
-struct msgbuf *msgp;
-int msgsz;
-long msgtyp;
-int msgflg;
-
+msgrcv(msqid, msgp, msgsz, msgtyp, msgflg)
+int msqid; struct msgbuf *msgp; int msgsz; long msgtyp; int msgflg;
 {
 	int parm[7];
 
-	if ( msgfno < 0 ) {
+	if (msgfno < 0) {
 		errno = ENODEV;
 		return -1;
 	}
@@ -128,6 +98,8 @@ int msgflg;
 	parm[5] = (int) (msgtyp >> 16);
 	parm[6] = msgflg;
 
-	ioctl( msgfno, MSGRCV, parm );
+	ioctl(msgfno, MSGRCV, parm);
 	return parm[0];
 }
+
+/* end of msgop.c */
