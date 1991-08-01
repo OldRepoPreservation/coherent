03d64ce3db27338b6b3094db4cf3fded39f6f01c
System Administrator <root@localhost>
1991-08-01 13:49:39 -0500

    updated by hal to include rts/cts handshaking.

--- kernel/USRSRC/i8086/drv/al.c
+++ kernel/USRSRC/i8086/drv/al.c
@@ -3,7 +3,10 @@
  * 	Copyright (c) 1982, 1990 by Mark Williams Company.
  * 	All rights reserved. May not be copied without permission.
  *
- * $Log:	/usr/src/sys/i8086/drv/RCS/al.c,v $
+ * $Log:	al.c,v $
+ * Revision 1.4  91/07/31  16:06:33  hal
+ * Change include usage.  Use AL[01]_MAJOR.
+ * 
  * Revision 1.2	91/02/21  11:21:28	hal
  * Used in COH Release 3.1.0 - add COM3/COM4 and polling
  * 
@@ -27,6 +30,7 @@
 #include <sys/ins8250.h>
 #include <sys/sched.h>
 #include <sys/al.h>
+#include <sys/devices.h>
 
 #define	minor_st(dev)	(dev & 0x3f)
 #define	DEV_TTY		(alttab[minor_st(dev)])
@@ -53,7 +57,7 @@
 #define ALNUMb	2		/* com3 has com number of 2 */
 #define ALINT	4		/* Interrupt level of com1_3 ports */
 #define	ALNAME	a0con		/* CON name of com1_3 ports */
-#define ALMAJ	5		/* Major number of com1_3 port */
+#define ALMAJ	AL0_MAJOR	/* Major number of com1_3 port */
 #define ALCNT	A0CNT		/* Number of ports for this IRQ */
 #define ALSPEEDa C1BAUD		/* Name of patchable variable for com1 speed */
 #define ALSPEEDb C3BAUD		/* Name of patchable variable for com3 speed */
@@ -66,7 +70,7 @@
 #define ALNUMb	3		/* com4 has com number of 3 */
 #define ALINT	3		/* Interrupt level of com2_4 ports */
 #define ALNAME	a1con		/* CON name of com2_4 ports */
-#define ALMAJ	6		/* Major number of com2_4 ports */
+#define ALMAJ	AL1_MAJOR	/* Major number of com2_4 ports */
 #define ALCNT	A1CNT		/* Number of ports for this IRQ */
 #define ALSPEEDa C2BAUD		/* Name of patchable variable for com2 speed */
 #define ALSPEEDb C4BAUD		/* Name of patchable variable for com4 speed */
@@ -144,7 +148,7 @@ alload()
 	extern int albaud[];
 	int port, i;
 
-	if ( init == 0
+	if (init == 0
 	  && (alttab = (TTY *)kalloc(ALCNT * sizeof(TTY)))
 	  && (ddp = (COM_DDP *)kalloc(ALCNT * sizeof(COM_DDP)))) {
 		kclear(alttab, ALCNT*sizeof(TTY));
@@ -166,18 +170,18 @@ alload()
 			ddp[1].com_num = ALNUMb;
 		}
 
-		for ( i = 0;  i < ALCNT; i++ ) {
+		for (i = 0;  i < ALCNT; i++) {
 			int speed = alttab[i].t_dospeed;
 
 			/* port = base I/O address */
 			port = ((COM_DDP *)(alttab[i].t_ddp))->port;
 			outb(port+IER, 0);	/* disable port interrupts */
-			if ( inb(port+IER) == 0 ) {
+			if (inb(port+IER) == 0) {
 				outb(port+MCR, 0);  /* hangup port */
 				outb(port+LCR, LC_DLAB);
-				outb(port+DLL, albaud[speed] );
-				outb(port+DLH, albaud[speed] >> 8 );
-				outb(port+LCR, LC_CS8 );
+				outb(port+DLL, albaud[speed]);
+				outb(port+DLH, albaud[speed] >> 8);
+				outb(port+LCR, LC_CS8);
 			}
 			alttab[i].t_start = alxstart;
 			alttab[i].t_param = alxparam;
@@ -185,7 +189,7 @@ alload()
 		}
 
 		setivec(ALINT, alintr);     /* set interrupt vector */
-		spl( s );
+		spl(s);
 	}
 }
 
@@ -194,13 +198,13 @@ alunload()
 {
 	int port, i;
 
-	for ( i = 0;  i < ALCNT; i++ ) {
+	for (i = 0;  i < ALCNT; i++) {
 		port = ((COM_DDP *)(alttab[i].t_ddp))->port;
 		outb(port+IER, 0);	/* disable port interrupts */
 		outb(port+MCR, 0);	/* hangup port */
 		timeout(alttab[i].t_rawtim, 0, NULL, 0);/* cancel timer */
 	}
-	clrivec( ALINT );			/* release interrupt vector */
+	clrivec(ALINT);			/* release interrupt vector */
 	kfree(alttab);
 	kfree(ddp);
 }
@@ -212,8 +216,8 @@ int	mode;
 {
 	if (minor_st(dev) < ALCNT) {
 		alload();
-		alxcycle( &DEV_TTY );
-		alxopen( dev, mode, &DEV_TTY, &irqtty);
+		alxcycle(&DEV_TTY);
+		alxopen(dev, mode, &DEV_TTY, &irqtty);
 	} else
 		u.u_error = ENXIO;
 }
@@ -227,7 +231,7 @@ int	mode;
 
 	if (--DEV_TTY.t_open == 0) {	/* Last open */
 		s = sphi();
-		alxclose( dev, mode, &DEV_TTY );
+		alxclose(dev, mode, &DEV_TTY);
 		spl(s);
 	}
 }
@@ -237,7 +241,7 @@ alread(dev, iop)
 dev_t	dev;
 IO	*iop;
 {
-	ttread(&DEV_TTY, iop, 0);
+	ttread(&DEV_TTY, iop);
 }
 
 static
@@ -250,29 +254,29 @@ register IO	*iop;
 	/*
 	 * Treat user writes through tty driver.
 	 */
-	if ( iop->io_seg != IOSYS ) {
-		ttwrite( &DEV_TTY, iop, 0 );
+	if (iop->io_seg != IOSYS) {
+		ttwrite(&DEV_TTY, iop);
 		return;
 	}
 
 	/*
 	 * Treat kernel writes by blocking on transmit buffer.
 	 */
-	while ( (c = iogetc(iop)) >= 0 ) {
+	while ((c = iogetc(iop)) >= 0) {
 		/*
 		 * Wait until transmit buffer is empty.
 		 * Check twice to prevent critical race with interrupt handler.
 		 */
 		for (;;) {
-			if ( inb(ALPORT+LSR) & LS_TxRDY )
-				if ( inb(ALPORT+LSR) & LS_TxRDY )
+			if (inb(ALPORT+LSR) & LS_TxRDY)
+				if (inb(ALPORT+LSR) & LS_TxRDY)
 					break;
 		}
 
 		/*
 		 * Output the next character.
 		 */
-		outb( ALPORT+DREG, c );
+		outb(ALPORT+DREG, c);
 	}
 }
 
@@ -285,12 +289,12 @@ struct sgttyb *vec;
 }
 
 static
-alpoll( dev, ev, msec )
+alpoll(dev, ev, msec)
 dev_t dev;
 int ev;
 int msec;
 {
-	return ttpoll( &DEV_TTY, ev, msec );
+	return ttpoll(&DEV_TTY, ev, msec);
 }
 
 static
