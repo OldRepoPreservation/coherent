6b3c228e59ad46f78a5bb39efaf225d92f1c4b7e
System Administrator <root@localhost>
1991-09-26 17:18:15 -0500

    stevesf fixed -1 option

--- bin/c.c
+++ bin/c.c
@@ -1,11 +1,13 @@
 /*
  * c.c
- * 2/14/91
+ * 9/23/91
  * Usage: c [ -lN ] [ -wN ] [ -012 ]
  * Columnize from stdin to stdout.
  */
 
 #include	<stdio.h>
+
+#define	VERSION	"1.3"
 
 extern	char	*getenv();
 
@@ -32,6 +34,7 @@ LINE	*ltail;				/* tail of LINE list		*/
 int	nacross;			/* # fields across page		*/
 int	ndown;				/* # fields down page		*/
 int	nfields;			/* # fields read from input	*/
+int	npp;				/* # fields per page		*/
 char	*(*select)() = sel1;		/* selector function		*/
 char	*usage = "\nUsage: c [ -lN ] [ -wN ] [ -012 ]";
 int	width = 80;			/* # columns across page	*/
@@ -44,10 +47,8 @@ main(argc, argv) int argc; char *argv[];
 	setflags(av);
 	setflags(&argv[1]);
 	setbuf(stdout, obuf);
-
 	input();
 	output();
-
 	exit(0);
 }
 
@@ -72,6 +73,9 @@ setflags(av) register char **av;
 			break;
 		case 'l':
 			length = atoi(&av[0][2]);
+			break;
+		case 'V':
+			fprintf(stderr, "c: V%s\n", VERSION);
 			break;
 		default:
 			fatal("bad option %s%s", av[0], usage);
@@ -116,9 +120,11 @@ output()
 	nacross = (width+1) / fieldmax;
 	if (nacross <= 0)
 		nacross = 1;
-	ndown = (nfields+nacross-1) / nacross;
-	for (i=0; i<ndown; ++i) {
-		for (j=0; j<nacross; ++j)
+	if (length != 0)
+		npp = length * nacross;
+	ndown = (nfields + nacross - 1) / nacross;
+	for (i = 0; i < ndown; ++i) {
+		for (j = 0; j < nacross; ++j)
 			putline((*select)(i, j));
 		putline((char *)0);
 	}
@@ -228,7 +234,7 @@ item(n) int n;
 
 	for (m = 0, lp = lhead; lp; lp = lp->l_next)
 		if (++m > n)
-			return (lp->l_line? lp->l_line: "");
+			return (lp->l_line ? lp->l_line : "");
 	return "";
 }
 
@@ -248,17 +254,17 @@ sel0(i, j) int i, j;
 char *
 sel1(i, j) int i, j;
 {
-	register int jx;
+	register int page, rows, line;
 
 	if (length == 0)
 		return item(i + j*ndown);
-	if (i/length == (ndown-1)/length)
-		jx = length;			/* rows on nonfinal page */
-	else if (ndown == length)
-		jx = length;			/* rows on full final page */
+	page = i / length;			/* page number */
+	line = i % length;			/* line on page */
+	if (page == (ndown-1)/length)
+		rows = ((ndown-1)%length) + 1;	/* rows on last page */
 	else
-		jx = ndown%length;		/* rows on final page */
-	return item((i/length)*length*nacross + i%length + j*jx);
+		rows = length;			/* rows on full page */
+	return item(page * npp + line + j * rows);
 }
 
 /*
@@ -268,17 +274,25 @@ sel1(i, j) int i, j;
 char *
 sel2(i, j) int i, j;
 {
-	if (i*nacross+j >= nfields)
+	register int ncols, page, line, adjust;
+
+	if (i * nacross + j >= nfields)
 		return "";
+	ncols = nfields % nacross;		/* columns in incomplete last line */
+	adjust = (j > ncols) ? ncols : j;	/* last line adjust */
 	if (length == 0)
-		return item(i + j*(ndown-1) + (j>nfields%nacross? nfields%nacross: j));
-	else if (i/length != (ndown-1)/length)	/* Not last page. */
-		return item((i/length)*length*nacross + i%length + j*length);
-	else					/* Last page. */
-		return item((i/length)*length*nacross	/* n on prev. pages. */
-			   + i%length		/* Add row. */
-			   + j*((ndown-1)%length)
-			   + ((j > nfields%nacross) ? nfields%nacross : j));
+		return item(i			/* n on prev. lines this col. */
+			   + j*(ndown-1)	/* n on prev. cols. */
+			   + adjust);		/* n on last line */
+	page = i / length;			/* page number */
+	line = i % length;			/* line on page */
+	if (page != (ndown-1)/length)		/* not last page */
+		return item(page * npp + line + j * length);
+	else					/* last page */
+		return item(page * npp		/* n on prev. pages. */
+			   + line		/* n on prev. lines this col. */
+			   + j*((ndown-1)%length)	/* n on prev. cols. */
+			   + adjust);		/* n on last line */
 }
 
 envargs(av) char **av;
