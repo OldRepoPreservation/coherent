0cad533121aca01f2c2b3f5d56a0a9cce7cfbc9c
Robert Swartz <rs@mwc.com>
1991-10-23 13:36:28 -0500

    now breaking the string record.date into the integers month, day and year. This will be used to accomodate a future option to specify a date when printing the file.

--- usr/bob/test/mwcbbs/print.c
+++ usr/bob/test/mwcbbs/print.c
@@ -15,7 +15,8 @@ WINDOW *win2;
 
 {
 FILE *infp, *outfp;
-char new_date[9];
+char new_date[3];
+int month, day, year;
 
 	if ((infp=(fopen(workfile,"r")))==NULL)
 		{
@@ -43,16 +44,22 @@ char new_date[9];
 		{
 		strcpy(new_date,"");
 		strncpy(new_date,record.date,2);
-		new_date[2] = '/';
-		new_date[3] = record.date[2];
-		new_date[4] = record.date[3];
-		new_date[5] = '/';
-		new_date[6] = record.date[4];
-		new_date[7] = record.date[5];
-		new_date[8] = '\0';
+		month = atoi(new_date);
+		strcpy(new_date,"");
 
-		fprintf(outfp,"\nFILE: %s\t\tDATE: %s\t\t\tSIZE: %s\n\n",
-			record.filename, new_date,record.filesize);
+		new_date[0] = record.date[2];
+		new_date[1] = record.date[3];
+		new_date[2] = '/';
+		day = atoi(new_date);
+		strcpy(new_date,"");
+
+		new_date[0] = record.date[4];
+		new_date[1] = record.date[5];
+		new_date[2] = '\0';
+		year = atoi(new_date);
+
+		fprintf(outfp,"\nFILE: %15s\t\tDATE: %d/%d/%d\t\tSIZE: %s\n\n",
+			record.filename, month, day, year ,record.filesize);
 		fprintf(outfp,"Description:\n");
 		fprintf(outfp," %s\n",record.description);
 		fprintf(outfp,"Other notes:\n");
@@ -64,7 +71,15 @@ char new_date[9];
 	fclose(outfp);
 	wclear(win2);
 	wmove(win2,0,0);
-	wprintw(win2,"File written. Press <RETURN> to continue.");
+	wprintw(win2,"Because we have just written the file %s,",PRINTFILE);
+	wmove(win2,1,0);
+	wprintw(win2,"the program will now end so that you may view the");
+	wmove(win2,2,0);
+	wprintw(win2,"file and/or print it. This will also prevent the");
+	wmove(win2,3,0);
+	wprintw(win2,"possibility of immediate accidental erasure of the file.");
+	wmove(win2,6,0);
+	wprintw(win2,"File written. Press <RETURN> to exit program.");
 	clrtoeol();
 	wrefresh(win2);
 	while(13 != wgetch(win2)) ;
