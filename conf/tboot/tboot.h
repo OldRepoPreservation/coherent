6e14829225246bb73da13528ba8a52ed7ea65c99
La Monte Yarroll <piggy@mwc.com>
1991-11-06 15:13:51 -0600

    Moved a lot of symbols here from tboot.c. Added a bunch of function declarations. Mostly a lot of additions.

--- conf/tboot/tboot.h
+++ conf/tboot/tboot.h
@@ -1,12 +1,25 @@
 /* Include file for tertiary boot programs.
  *
+ * This is a real hodge-podge of symbols.  If you are looking to improve
+ * code readability, start by hacking this file into many tiny pieces.
+ *
  * La Monte H. Yarroll <piggy@mwc.com>, September 1991
  */
 
+#ifdef MAIN
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
+
 #include <sys/types.h>
+#include <sys/buf.h>
+#include "ptypes.h"
 
 #define TRUE	(1==1)
 #define FALSE	(1==2)
+#define WS	" \t"
+#define BIGINT	((int32) 65535L)	/* Largest unsigned int.  */
 #define	NULL	((char *)0)
 #define	FOURK	0x1000	/* 4k page.  Needed for alignment purposes.  */
 #define FOURKBOUNDRY	0xf000
@@ -25,11 +38,45 @@
 
 #define INODES_PER_BLOCK 8
 
+
+#ifndef	NHD
+#define	NHD	1			/* # of heads per drive [1 for f9d0]. */
+#endif
+
+#ifndef	NSPT
+#define	NSPT	9			/* # of sectors per track on floppy. */
+#define	NTRK	40			/* # of tracks on floppy. */
+#endif
+
+#define ROOTINO 2			/* Root inode # */
+#define INOORG	2			/* First inode block. */
+#define IBSHIFT 3			/* Shift, inode to blocks */
+#define IOSHIFT 6			/* Shift, inode to bytes */
+#define INOMASK 0x0007			/* Mask, inode to offset */
+#define BUFSIZE 512			/* Block size. */
+#define DISK	0x13			/* Disk Interrupt */
+#define KEYBD	0x16			/* Keyboard Interrupt */
+#define READ1	0x0201			/* read one sector */
+#define FIRST	8			/* Relative start of partition. */
+#define FULLSEG	0xffff			/* Size of a whole 8086 segment. */
+#define PPMASK	(unsigned short) 0xfff0 /* Mask for rounding to paragraph.  */
+
+#define COFF_SYS_BASE	0x0200		/* System load base paragraph for 386.  */
+#define DEF_SYS_BASE	0x0060		/* System load base paragraph. */
+#define SYS_START	0x0100		/* System entry point. */
+
+#define THE_DEV		((dev_t)0x01)	/* The one disk device we recognize.  */
+
+/* WAIT_DELAY is how long to wait after finding autoboot before booting.  */
+#define WAIT_DELAY	182	/* 10 seconds * 18.2 clicks per second.  */
+
+/* Useful macros.  */
 #define GREATEST(a, b, c) (a > (b>c?b:c) ? a : (b>c?b:c))
 #define LESSER(a, b) (a < b ? a : b)
 #define HIGH(x)	(x >> 8)	/* High byte of 16 bit number.  */
 #define LOW(x)	(x & 0xff)	/* Low byte of 16 bit number.  */
 
+/* Register structure used by call_bios().  */
 struct reg {
 	unsigned r_ax;
 	unsigned r_bx;
@@ -46,8 +93,8 @@ struct reg {
 struct load_segment {
 	int valid;			/* Is this a valid table entry?	*/
 	char *message;			/* Message to print while loading.  */
-	unsigned short load_toseg;	/* Where in memory to		*/
-	unsigned short load_tooffset;	/* load this segment.		*/
+	uint16 load_toseg;	/* Where in memory to		*/
+	uint16 load_tooffset;	/* load this segment.		*/
 	fsize_t load_offset;	/* Where in file to get it.	*/
 	fsize_t load_length;	/* How long it is.		*/
 };
@@ -59,7 +106,7 @@ extern char *gets();	/* Get a string from the keyboard.  */
 extern void reverse();	/* Reverse a string in place.  */
 extern void itoa();	/* Convert an integer to a decimal string.  */
 extern void itobase();	/* Convert an integer to an arbitrary base string.  */
-extern unsigned int basetoi(); /* Convert an arbitrary base string to an integer.  */
+extern uint16 basetoi(); /* Convert an arbitrary base string to an integer.  */
 extern int abread();	/* Read a physical block into an unaligned buffer.  */
 extern ino_t namei();	/* Convert from a name to an inode.  */
 extern void iread();	/* Read from a file, given an inode.  */
@@ -71,4 +118,23 @@ extern int coff2lout();	/* Generate an l.out header for a COFF file.  */
 extern void dpb();	/* Display parameters from bios.  */
 extern void dir();	/* List contents of /.  */
 extern char *lpad();	/* Pad a string on the left.  */
-extern unsigned short sys_base;	/* System load base paragraph.  */
+extern uint16 object_nlist();	/* Look up a symbol in an object file.  */
+extern uint16 object_sys_base(); /* Generate a default sys_base.  */
+extern uint32 wrap_coffnlist();	/* Candy coated coff nlist().  */
+extern int wait_for_keystrok();	/* Wait a time delay for a keystroke.  */
+extern BUF *bread();		/* Read a disk block.  */
+extern BUF *bclaim();		/* Claim a disk buffer.  */
+extern BUF *bpick();		/* Pick a buffer to trash.  */
+extern void bufinit();		/* Initialize disk buffers.  */
+extern int better_buf();	/* Compare the stealability of two buffers.  */
+extern void brelease();		/* Free a disk buffer.  */
+extern int gate_lock();		/* Attempt to lock a GATE.  */
+extern int gate_locked();	/* Check to see if a GATE is locked.  */
+extern void gate_unlock();	/* Unlock a GATE.  */
+extern void print32();		/* Print a 32 bit integer, base 16.  */
+extern void sanity_check();	/* Check for insane conditions.  */
+extern void seg_align();	/* Align a far address.  */
+
+extern int errno;	/* Error number for "system" calls.  */
+EXTERN uint16 sys_base;	/* Segment into which to load the kernel.  */
+EXTERN int sys_base_set;	/* Has sys_base been explicitly set?  */
