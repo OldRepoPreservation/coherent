60c285c25f9bd3da33fbb6a5627ecb1885087e6b
Hal Snyder <hal@mwc.com>
1991-11-14 14:12:33 -0600

    Re-entrancy fix.  Change type of com_usage.

--- 286_KERNEL/USRSRC/286/support.c
+++ 286_KERNEL/USRSRC/286/support.c
@@ -3,7 +3,7 @@
  * the following kernel resident parts are shared by loadable serial drivers
  *   (see al.h and poll_clk.h)
  */
-int	com_usage[NUM_AL_PORTS];	/* COM_UNUSED/COM_IRQ/COM_POLLED */
+com_usage_t	com_usage[NUM_AL_PORTS];	/* see poll_clk.h */
 TTY	*(tp_table[NUM_AL_PORTS]);	/* table of pointers for polling */
 int	poll_rate;			/* poll_* variables are explained */
 int	poll_owner;			/* in poll_clk.h */
