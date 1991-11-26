6c397c3e3827665f8b4cc8781498797922961d4c
La Monte Yarroll <piggy@mwc.com>
1991-11-26 15:05:34 -0600

    Revised boot message to make installation less confusing. Updated version number to go with new boot gift--T_BIOS_ROOTDEV.

--- conf/old_tboot/tboot.c
+++ conf/old_tboot/tboot.c
@@ -54,7 +54,7 @@ main()
 	sys_base = DEF_SYS_BASE;
 	sys_base_set = FALSE;
 
-	puts("\r\nCOHERENT Tertiary boot Version 1.0.1 alpha\r\n");
+	puts("\r\nCOHERENT Tertiary boot Version 1.0.2 alpha\r\n");
 	/* Look for a valid executable.  */
 	do {
 		/* Find the file in the file system.  */
@@ -67,6 +67,7 @@ main()
 				puts("\r\nCan't find \"");
 				puts(cmd_name);
 				puts("\".  Please choose another.\r\n");
+				puts("If installing COHERENT, please type begin.\r\n");
 			}
 
 			/* Fetch new file names, executing them
