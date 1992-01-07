3c49d1b05dfc1a687a1359d9e70e95b53d725422
System Administrator <root@localhost>
1992-01-07 08:19:53 -0600

    Vlad fixed various problems to make chgrp match our docs.

--- bin/chgrp.c
+++ bin/chgrp.c
@@ -2,9 +2,15 @@
  * Change the group owner of specified files.
  */
 
+/*
+ * chgrp didn't check numerical group id; prints out strange
+ * error messages, didn't check effective uid. Fixed 1-06-91. Vlad.
+ */
+
 #include <stdio.h>
 #include <sys/stat.h>
 #include <grp.h>
+#include <ctype.h>	/* Vlad */
 
 main(argc, argv)
 char *argv[];
@@ -15,15 +21,23 @@ char *argv[];
 	struct stat sb;
 	register short status = 0;
 
+	/* Only superuser can invoke chgrp under COHERENT. Vlad */
+	if (geteuid())
+		cherr("not a super user\n");
+
 	if (argc < 3)
 		usage();
-	if ((c = *argv[1])>='0' && c<='9')
-		group = atoi(argv[1]);
-	else {
+	
+	/* You can specify group by numeric id or by name. Vlad */
+	if (isdigit((int) *argv[1])) {
+		if ((grp = getgrgid(atoi(argv[1]))) == NULL)
+			cherr("bad numeric group id `%s'\n", argv[1]);
+	} else
 		if ((grp = getgrnam(argv[1])) == NULL)
-			cherr("Bad username `%s'\n", argv[1]);
-		group = grp->gr_gid;
-	}
+			cherr("bad group name `%s'\n", argv[1]);
+
+	group = grp->gr_gid;
+
 	for (c = 2; c < argc; c++) {
 		owner = 0;
 		if (stat(argv[c], &sb) >= 0)
@@ -48,7 +62,3 @@ cherr(x)
 	fprintf(stderr, "chgrp: %r", &x);
 	exit(2);
 }
-
-
-
-
