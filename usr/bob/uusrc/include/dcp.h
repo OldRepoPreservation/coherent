899dcce13fe979374cf5c086f76c688f602b7cd6
System Administrator <root@localhost>
1992-01-27 10:01:04 -0600

    bob h bumped version # to 3.2.24 for t[the sepcline() fix.

--- usr/bob/uusrc/include/dcp.h
+++ usr/bob/uusrc/include/dcp.h
@@ -15,7 +15,7 @@
  *  Various definitions of Global Strings and Values
  */
 
-#define	VERSION		"3.2.23"
+#define	VERSION		"3.2.24"
 
 #define PUBDIR		"/usr/spool/uucppublic"
 #define SPOOLDIR	"/usr/spool/uucp"
