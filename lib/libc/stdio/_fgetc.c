792c2ba3cc86cd584702db0f5ad839d7eb820a74
System Administrator <root@localhost>
1992-02-14 21:00:20 -0600

    steve 2/14/92 Preserve errno if read succeeds.

--- lib/libc/stdio/_fgetc.c
+++ lib/libc/stdio/_fgetc.c
@@ -1,32 +1,41 @@
 /*
- * Standard I/O Library Internals
- * Unbuffered input
+ * libc/stdio/_fgetc.c
+ * C Standard I/O Library internals.
+ * Unbuffered input.
  */
 
 #include <stdio.h>
 #include <errno.h>
 
-int
-_fgetc(fp)
-register FILE	*fp;
-{
-	register unsigned char	s[1];
-	extern	int	_fputt();
+extern	int	_fputt();
 
-	if (stdout->_pt==&_fputt)		/* special kludge */
+int
+_fgetc(fp) register FILE *fp;
+{
+	register unsigned char s[1];
+	register int n, oerrno;
+
+	if (stdout->_pt==&_fputt)	/* special kludge */
 		fflush(stdout);
 	fp->_cc = 0;
+	oerrno = errno;			/* save old errno */
 	errno = 0;
+	n = EOF;			/* return value in case error */
 	switch (read(fileno(fp), s, 1)) {
-	case -1:
+	case -1:			/* error */
 		if (errno != EINTR)
 			fp->_ff |= _FERR;
 		break;
-	case 0:
+	case 0:				/* EOF */
 		fp->_ff |= _FEOF;
 		break;
-	default:
-		return (s[0]);
+	default:			/* success */
+		n = s[0];
+		break;
 	}
-	return (EOF);
+	if (errno == 0)
+		errno = oerrno;		/* preserve errno if no error */
+	return n;
 }
+
+/* end of libc/stdio/_fgetc.c */
