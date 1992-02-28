b6e7760b98d22485ca3b16d814fb6e44a1985e23
System Administrator <root@localhost>
1992-02-28 11:29:49 -0600

    *** empty log message ***

---
+++ conf/patch/patch.h
@@ -0,0 +1,34 @@
+#include <coff.h>
+
+/* The PLIST struct describes all aspects of a single patch operation.  */
+typedef struct plist {
+ 	SYMENT *p_lvnp; /* nlist entry for LHS of assignment.  */
+	SYMENT *p_rvnp; /* nlist entry for RHS of assignment.  */
+	long	p_lval; /* Value of LHS of assignment.  */
+	long	p_rval; /* Value of RHS of assignment.  */
+	int	p_type; /* Type of data being patched.  Can be one of:
+			 * 'c' - character
+			 * 's' - short
+			 * 'i' - integer
+			 * 'l' - long
+			 */
+	union {
+		char	p_char;
+		short	p_short;
+		int	p_int;
+		long	p_long;
+	} p_val;
+} PLIST;
+
+
+void main();
+int getnames();
+void badsym();
+void getone();
+void setfile();
+void setkmem();
+int patch();
+long myatol();
+void usage();
+char *index();
+char *realloc();
