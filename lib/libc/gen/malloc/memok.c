3aa91e673da3ac3c54f26f06b9041abe13d913ae
System Administrator <root@localhost>
1992-05-06 22:14:15 -0500

    steve 5/6/92 Added test against __a_first to avoid core dumping on corrupt arenas in i386.

--- lib/libc/gen/malloc/memok.c
+++ lib/libc/gen/malloc/memok.c
@@ -1,13 +1,14 @@
 /*
- * memok.c
+ * libc/gen/malloc/memok.c
  * Test for corruption of malloc arena.
+ * Assumes contiguous allocation from __a_first to __a_top.
  */
 
 #include <stdio.h>
 #include <sys/malloc.h>
 
 /*
- * memok() returns 1 if memory is ok,
+ * Return 1 if malloc arena is ok,
  * 0 if it is corrupted.
  */
 int
@@ -22,7 +23,11 @@ memok()
 	for (counter = __a_count; counter--; ) {
 		if (mp == __a_scanp)
 			flag++;
+		if ((mp >= __a_top) || (mp < __a_first))
+			return 0;	/* out of bounds */
 		mp = (len = realsize(mp->blksize)) ? bumpp(mp, len) : mp->uval.next;
 	}
 	return ((1 == flag) && (mp == __a_first));
 }
+
+/* end of memok.c */
