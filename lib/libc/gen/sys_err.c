bd940744ba5e3f31a5c6915669059ded8ef80f49
System Administrator <root@localhost>
1992-06-04 20:10:58 -0500

    steve 6/4/92 Added EDEADLK and ENOLCK messages. Rearranged condionalization in keeping with parallel changes to <errno.h>.

--- lib/libc/gen/sys_err.c
+++ lib/libc/gen/sys_err.c
@@ -38,13 +38,13 @@ char	*sys_errlist[] = {
 	"broken pipe",
 	"math argument",
 	"result too large",
-#ifndef	_I386
 	"out of kernel space",
 	"driver not loaded",
 	"bad exec format",
 	"device needs attention",
-	"device busy"
-#endif
+	"device busy",
+	"deadlock",
+	"no lock available"
 };
 
 int	sys_nerr = sizeof (sys_errlist)/sizeof (sys_errlist[0]);
