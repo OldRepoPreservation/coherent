873498ade4975dfdeb3cb503bdde9cb0bba3a970
System Administrator <root@localhost>
1992-06-10 21:35:22 -0500

    steve 6/10/92 Rather than returning x below THRESHOLD, expand according to Taylor's Series.

--- lib/libm/sin.c
+++ lib/libm/sin.c
@@ -9,24 +9,29 @@
 #include "emumath.h"
 #endif
 
+#define	THRESHOLD	1.111111111111111111e-3
+
 /*
- * To get a correct result for very small |x|,
- * the code below just returns x for |x| < THRESHOLD.
- * We can derive a theoretical value for THRESHOLD from the series:
- *	sin(x)  = x - x^3/3! + x^5/5! - ...
- * so if |x| < sqrt(6 * DBL_EPSILON) then x^3/3! < x * DBL_EPSILON
- * and the low-order terms must be insignificant.
- * The threshold value below, arrived at empirically, is somewhat larger;
- * it is for IEEE fp, the DECVAX value must be slightly different but...
+ * When |x| is very small, use the series
+ *	x - x**3 / 6 + x**5 / 120 - ...
+ * to get an accurate result while avoiding loss of precision.
+ * The THRESHOLD value above is determined empirically.
  */
-#define	THRESHOLD	2.1485600010223542e-8
+static readonly double sintab[] ={
+	 0.000000000000000000000000,
+	 1.000000000000000000000000,
+	 0.000000000000000000000000,
+	-0.166666666666666666666666,
+	 0.000000000000000000000000,
+	 0.008333333333333333333333
+};
 
 double
 sin(x) double x;
 {
 	if (fabs(x) < THRESHOLD)
-		return x;
-	return cos(x - PI/2.0);
+		return _pol(x, sintab, 6);
+	return cos(PI/2.0 - x);
 }
 
 /* end of sin.c */
