78bba0f2c85257dcabcea8a2fd4219ab4cee992c
System Administrator <root@localhost>
1992-07-17 09:39:31 -0500

    update bystevesf for profiling suppt

--- lib/libc/crt/_prof.c
+++ lib/libc/crt/_prof.c
@@ -1,37 +1,54 @@
 /*
- * Called from the run-time start-off to enable profiling and to
- * write out the final profiling information.
+ * libc/crt/_prof.c
+ * Runtime profiling.
  */
+
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 
+#if	_I386
+extern	_start();
+extern	__end_text();
+#define	BTEXT	_start
+#define	ETEXT	__end_text
+#else
+extern	etext();
+#define	BTEXT	(vaddr_t)1
+#define	ETEXT	etext
+#endif
 
-#define	STDERR	2
-
-
+/*
+ * The profiling runtime start-off mcrts0.s
+ * enables profiling by calling _profon() before calling main().
+ */
 _profon()
 {
 #ifndef	Z8001
 	register unsigned	bufl;
 	register short		*buf;
-	extern			etext();
 	static char		emsg[]	= "No room for profil buffer\n";
 
-	bufl = (unsigned)etext / 2;
+	bufl = ((unsigned)ETEXT - (unsigned)BTEXT) / 2;
 	buf = (short *)sbrk(bufl * sizeof (short));
 	if (buf == NULL) {
-		write(STDERR, emsg, sizeof emsg);
+		write(STDERR_FILENO, emsg, sizeof emsg);
 		abort();
 	}
-	monitor((vaddr_t)1, (vaddr_t)etext, buf, bufl);
+	monitor((vaddr_t)BTEXT, (vaddr_t)ETEXT, buf, bufl);
 #else
 	monitor((vaddr_t)1);		/* dummy call to start things off */
 #endif
 }
 
-
+/*
+ * The profiling runtime start-off mcrts0.s
+ * disables profiling by calling _profoff() at _exit.
+ * It turns off profiling and writes out the final profiling information.
+ */
 _profoff()
 {
 	monitor(NULL);
 }
 
+/* end of libc/crt/_prof.c */
