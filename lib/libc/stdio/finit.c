7ab8f01ea5aab24eedb9a936c979b3c8aae2224d
System Administrator <root@localhost>
1992-07-27 17:35:44 -0500

    steve 7/27/92 Preserve errno in finit(); isatty() sets it to ENOTTY if false.

--- lib/libc/stdio/finit.c
+++ lib/libc/stdio/finit.c
@@ -1,6 +1,7 @@
 /*
- * Standard I/O Library Internals
- * initialize file for I/O
+ * libc/stdio/finit.c
+ * Standard i/o library internals.
+ * Initialize file for i/o:
  *	decide if buffered
  *	allocate buffer if required
  *	stuff appropriate get/put functions
@@ -8,19 +9,22 @@
  */
 
 #include <stdio.h>
+#include <errno.h>
 
+extern	long	lseek();
+
+extern	int	_fgetc();
+extern	int	_fputc();
+extern	int	_fgetb();
+extern	int	_fputb();
+extern	int	_fputt();
 
 void
-finit(fp)
-register FILE	*fp;
+finit(fp) register FILE *fp;
 {
-	register int	tty;
-	extern	int	_fgetc();
-	extern	int	_fputc();
-	extern	int	_fgetb();
-	extern	int	_fputb();
-	extern	int	_fputt();
+	register int tty, sav;
 
+	sav = errno;			/* because isatty() can set errno */
 	if (fp->_bp == NULL
 	 && (fp->_ff&_FSTBUF || (tty=isatty(fileno(fp))) && fp!=stdout
 	    || (fp->_bp = malloc(BUFSIZ)) == NULL)) {
@@ -37,50 +41,48 @@ register FILE	*fp;
 		fp->_dp = fp->_cp = fp->_bp + boffset(fileno(fp));
 		fp->_cc = 0;
 	}
+	errno = sav;
 }
 
 static
 int
-boffset(fd)
-int	fd;
+boffset(fd) int fd;
 {
-	extern	long	lseek();
-	register long	off;
+	register long off;
 
 	if ((off=lseek(fd, 0L, SEEK_CUR))==-1L)
-		return (0);
+		return 0;
 	else
-		return ((unsigned)off%BUFSIZ);
+		return (unsigned)off%BUFSIZ;
 }
 
 int
-_fginit(fp)
-register FILE	*fp;
+_fginit(fp) register FILE *fp;
 {
 	finit(fp);
-	return ((*fp->_gt)(fp));
+	return (*fp->_gt)(fp);
 }
 
 int
-_fpinit(c, fp)
-register char	c;
-register FILE	*fp;
+_fpinit(c, fp) register char c; register FILE *fp;
 {
 	finit(fp);
-	return ((*fp->_pt)(c, fp));
+	return (*fp->_pt)(c, fp);
 }
 
 
 /*
- * Close all files (called from exit)
+ * Close all files.
+ * Called from exit().
  */
-
 void
 _finish()
 {
-	register FILE	**fpp;
+	register FILE **fpp;
 
 	for (fpp = &_fp[_NFILE-1]; fpp>=&_fp[0]; --fpp)
-		if (*fpp!=NULL)
+		if (*fpp != NULL)
 			fclose(*fpp);
 }
+
+/* end of libc/stdio/finit.c */
