28a1ccf25cfdb5ac039a0c2060c95913cc9a4fe8
System Administrator <root@localhost>
1992-10-01 12:01:41 -0500

    update by stevesf post 401a

--- lib/libc/gen/ttyslot.c
+++ lib/libc/gen/ttyslot.c
@@ -1,22 +1,24 @@
 /*
+ * libc/gen/ttyslot.c
  * Coherent I/O Library.
- * Return the index of the entry in the
- * `/etc/ttys' file for this process.
+ * Return the index of the entry in the /etc/ttys file for this process.
  * Slot 0 is returned on error.
  * [Note: the controlling terminal of the process
  * is taken to be stderr].
- * We use fttyslot which returns -1 on error
+ * We use _ttyslot which returns -1 on error
  * for the benefit of ttyname().
  */
 
-int	fttyslot();		/* In ttyname.c */
+int	_ttyslot();		/* in libc/gen/ttyname.c */
 
+int
 ttyslot()
 {
 	register int slot;
 
-	if ((slot = fttyslot(2)) < 0)
-		slot += 1;
-	return (slot);
+	if ((slot = _ttyslot(2)) < 0)
+		++slot;
+	return slot;
 }
 
+/* end of libc/gen/ttyslot.c */
