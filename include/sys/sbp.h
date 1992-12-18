7f6c1d64b75dbe0815eba1d41ef7bc8bafc2b43d
System Administrator <root@localhost>
1992-12-18 14:05:16 -0600

    *** empty log message ***

---
+++ include/sys/sbp.h
@@ -0,0 +1,31 @@
+/*
+ * Constants for Sound Blaster Pro
+ */
+#ifndef _SBP_H
+#define _SBP_H
+
+/* i/o addresses */
+#define LEFT_FM_STATUS		(SBP_BASE)	/* read */
+#define LEFT_FM_ADDR		(SBP_BASE + )
+#define LEFT_FM_DATA		(SBP_BASE + )
+#define RIGHT_FM_STATUS		(SBP_BASE + )
+#define RIGHT_FM_ADDR		(SBP_BASE + )
+#define RIGHT_FM_DATA		(SBP_BASE + )
+#define MIXER_ADDR		(SBP_BASE + )
+#define MIXER_DATA		(SBP_BASE + )
+#define DSP_RESET		(SBP_BASE + )
+#define FM_STATUS		(SBP_BASE + )
+#define FM_ADDR			(SBP_BASE + )
+#define FM_DATA			(SBP_BASE + )
+#define DSP_READ_DATA		(SBP_BASE + )
+#define DSP_WR_DATA		(SBP_BASE + )
+#define DSP_WR_CMD		(SBP_BASE + )
+#define DSP_WBUF_STATUS		(SBP_BASE + )
+#define DSP_DATA_AVAIL		(SBP_BASE + )
+#define CD_DATA			(SBP_BASE + )
+#define CD_COMMAND		(SBP_BASE + )
+#define CD_STATUS		(SBP_BASE + )
+#define CD_RESET		(SBP_BASE + )
+#define CD_ENABLE		(SBP_BASE + )
+
+#endif
