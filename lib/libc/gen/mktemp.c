ff4ef4ad499aaeaf16ed6ec3d5af101d564e0506
System Administrator <root@localhost>
1993-01-05 16:26:55 -0600

    steve 1/5/93 Revised to avoid unlikely characters in generated filename.

--- lib/libc/gen/mktemp.c
+++ lib/libc/gen/mktemp.c
@@ -1,50 +1,38 @@
 /*
+ * libc/gen/mktemp.c
  * Produce a unique filename by replacing
  * the trailing 6 X's on the user's input string
  * with process id and a unique letter.
  * (It is assumed, but not checked, that the X's are present.)
- * Revised by Michael Griffin, 7-5-84.  Avoids control characters in
- * generator.  Repeats after 91 invocations by the same process.
+ * Revised by Michael Griffin, 7-5-84; by steve, 1/5/93.
+ * Avoids unsuitable characters in generator.
+ * Repeats after 26+26+10+1=63 invocations by the same process.
  */
 
-static	char	generator = 'a';
-
 char *
-mktemp(template)
-char *template;
+mktemp(template) char *template;
 {
+	static char generator = 'a';
 	register char *p;
-	register i, pid;
+	register int i, pid;
+
 
 	for (p = template; *p; p++)
-		;
-	switch (generator)  {
-		case '~':
-			*--p = generator;
-			generator = 'A';
-			break;
-		case '_':
-			*--p = generator;
-			generator = '!';
-			break;
-		case '*':
-			*--p = ++generator;
-			generator++;
-			break;
-		case '?':
-			*--p = (generator = '@');
-			break;
-		case '@':
-			generator = 'a';
-			*--p = generator++;
-			break;
-		default:
-			*--p = generator++;
+		;				/* scan to end of template */
+	*--p = generator;			/* store generator */
+	switch (generator)  {			/* bump generator */
+	case 'z':	generator = 'A';		break;
+	case 'Z':	generator = '0';		break;
+	case '9':	generator = '_';		break;
+	case '_':	generator = 'a';		break;
+	default:	++generator;			break;
 	}
 	pid = getpid();
-	i = 5; do {
+	for (i = 5; i-- > 0; ) {		/* store 5-digit ASCII pid */
 		*--p = pid%10 + '0';
 		pid /= 10;
-	} while (--i);
-	return (template);
+	}
+	return template;
 }
+
+/* end of libc/gen/mktemp.c */
