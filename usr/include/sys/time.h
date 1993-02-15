3430998d69ffce5150615c882d58f15240db5537
System Administrator <root@localhost>
1993-02-15 12:00:15 -0600

    norm: changes for gcc release

--- usr/include/sys/time.h
+++ usr/include/sys/time.h
@@ -1,4 +1,15 @@
+/* (-lgl
+ * 	COHERENT Version 4.0
+ * 	Copyright (c) 1982, 1992 by Mark Williams Company.
+ * 	All rights reserved. May not be copied without permission.
+ -lgl) */
+
+#ifndef	_TIME_H
+#define	_TIME_H
+
 struct timeval {
 	long tv_sec;
 	long tv_usec;
 };
+
+#endif
