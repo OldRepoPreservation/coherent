a9666b4e4744fa7a147c1325de9d432d3e958ea7
System Administrator <root@localhost>
1993-04-20 10:00:32 -0500

    kernel 77: vlad: sem_undo and other changes

--- kernel/io.386/sem386.c
+++ kernel/io.386/sem386.c
@@ -3,7 +3,10 @@
  *
  * Purpose: This module provides System V compatible semaphore operations.
  *
- * $Log$
+ * $Log:	sem386.c,v $
+ * Revision 1.1  93/04/09  08:48:23  bin
+ * Initial revision
+ * 
  */
 
 /*
@@ -41,7 +44,8 @@
 int	iSemPerm();	/* Check permissions */
 int	iSanityCheck();	/* Sanity check */
 int	iSemInit();	/* Init semaphores */
-void	vSemAdj();	/* Set sem adjust */
+void	vClearAdj();	/* Clear the adjust value */
+int	iSubAdj();	/* Subtract value from the sem adjust */
 /*
  * ----------------------------------------------------------------------
  * Global Data.
@@ -136,7 +140,13 @@ int 	nsems,		/* # of semaphores in the set */
 		return semidp - semids;
 	}
 
-	/* Set does not exist. So, we have to creat it */
+	/* Set does not exist. So, we have to create it */
+	/* Now nsems should be > 0 */
+	if (nsems < 1) {
+		unlock(gSemGate);
+		u.u_error = EINVAL;
+		return;
+	}
 
 	/* Check the total number of semaphores */
 	if ((iSemTotal + nsems > SEMMNS)) {
@@ -190,9 +200,9 @@ int 	nsems,		/* # of semaphores in the set */
 	 * SVR3 does not do it and suggests set up sem struct using
 	 * semctl() call. SVR4 manual says nothing about it.
 	 */
-	for (semp = semidp->sem_base; semp < semidp->sem_base + nsems; semp++)
+	for (semp = semidp->sem_base; semp < semidp->sem_base + nsems; semp++){
 		semp->semval = semp->sempid = semp->semncnt = semp->semzcnt = 0;
-
+	}
 	unlock(gSemGate);
 	return semidp - semids;
 }
@@ -275,7 +285,9 @@ union semun {
 			if (semp->semzcnt)
 				wakeup(&semp->semzcnt);
 		}
-		vSemAdj(semid, semnum, 0);
+		semidp->sem_ctime = timer.t_time;
+		/* Clear corresponding adjust value in all processes */
+		vClearAdj(semid, semnum);
 		return 0;
 	case GETPID:		/* Return value of sempid */
 		if (iSemPerm(semidp, usSEM_R))	/* cannot read */
@@ -310,19 +322,25 @@ union semun {
 		/* Set semvals accoding to the arg.array */
 		semp  = semidp->sem_base;
 		pusUserAr = arg.array;
+		lock(gSemGate);
 		for (i = 0; i < semidp->sem_nsems; i++) {
-			val = getusd(arg.array);
-			if (u.u_error)
+			val = getusd(pusUserAr++);
+			if (u.u_error) {
+				unlock(gSemGate);
 				return;
+			}
 			if (val < 0 || val > SEMVMX) {
 				u.u_error = ERANGE;
+				unlock(gSemGate);
 				return;
 			}
 			semp->semval = val;
-			vSemAdj(semid, i, 0);
-			pusUserAr++;
+			/* Clear corresponding adjust value in all processes. */
+			vClearAdj(semid, i);
 			semp++;
 		}
+		semidp->sem_ctime = timer.t_time;
+		unlock(gSemGate);
 		return 0;
 	case IPC_STAT:
 		if (iSemPerm(semidp, usSEM_R))	/* cannot read */
@@ -336,6 +354,7 @@ union semun {
 		semidp->sem_perm.gid   = getusd(&((arg.buf)->sem_perm.gid));
 		semidp->sem_perm.mode  =
 			(getusd(&((arg.buf)->sem_perm.mode))&0777) | IPC_ALLOC;
+		semidp->sem_ctime = timer.t_time;
 		return 0;
 	case IPC_RMID:
 		if ((u.u_uid != 0) && (u.u_uid != semidp->sem_perm.uid)
@@ -352,6 +371,7 @@ union semun {
 			if (semp->semzcnt)
 				wakeup(&semp->semzcnt);
 		}
+		/* We do not cleane up adjust values here */
 		iSemTotal -= semidp->sem_nsems;
 		semidp->sem_perm.mode = 0;
 		kfree(semidp->sem_base);
@@ -373,10 +393,11 @@ unsigned 	uNsops;		/* # of elements in the array */
 	register struct semid_ds	*rpstSemSet;	/* Semaphore set */
 	register struct sem 		*rpstSem;	/* Semaphore */
 	struct sembuf 			*pstSemBuf;	/* Operations */
-	unsigned short 			usSemNum, 	/* Semaphore number */
-					usSemFlg;
+	unsigned short 			usSemNum; 	/* Semaphore number */
+	short				sSemFlg;	/* Semaphore flag */
 	short 				sSemOper; 	/* Operation */
-	int				i;		/* Loop */
+	int				i;		/* Loop index */
+	short				change = 0;	/* Sem was changed */
 
 	/* Check if semids was alloced */
 	if (semids == 0) {
@@ -407,14 +428,20 @@ TRY_AGAIN:	/* Repeat the semaphore set */
 	/* do semaphore ops  */
 	for (i = 0, pstSemBuf = pstSops; i < uNsops; i++, pstSemBuf++) {
 		usSemNum = getusd(&(pstSemBuf->sem_num));
-		sSemOper  = getusd(&(pstSemBuf->sem_op));
-		usSemFlg = getusd(&(pstSemBuf->sem_flg));
+		sSemOper = getusd(&(pstSemBuf->sem_op));
+		sSemFlg = getusd(&(pstSemBuf->sem_flg));
 
+		if (sSemOper < -SEMVMX || sSemOper > SEMVMX) {
+			semundo(rpstSemSet, pstSops, i);
+			u.u_error = E2BIG;
+			unlock(gSemGate); 
+			return;
+		}
 		if ((u.u_error != 0) || (usSemNum >= rpstSemSet->sem_nsems)) {
 			/* We have falure here. undo all previous 
 			 * operations.
 			 */
-			semundo(rpstSemSet->sem_base, pstSops, i);
+			semundo(rpstSemSet, pstSops, i);
 			/* If u_error was not set it means that sem_number 
 			 * is bad. So, set error to EFBIG.
 			 */
@@ -431,7 +458,11 @@ TRY_AGAIN:	/* Repeat the semaphore set */
 		 * sSemOper == 0, & sSemOper > 0.
 		 */
 		if (sSemOper < 0) {	/* want to decrement semval */
-			/* We do not have to undo anythig, if we cannot alter*/	
+			/* We do not have to undo anything. If we cannot alter
+			 * we did not change any value in the set. But we
+			 * have to check here because other requests could be
+			 * for read (sSemOper=0)
+			 */	
 			if (iSemPerm(rpstSemSet, SEM_A)) { /* cannot alter */
 				unlock(gSemGate); 
 				return;
@@ -445,18 +476,24 @@ TRY_AGAIN:	/* Repeat the semaphore set */
 				if (!(rpstSem->semval += sSemOper))
 		 			if (rpstSem->semzcnt)
 						wakeup(&rpstSem->semzcnt);
+				if (sSemFlg & SEM_UNDO) {
+					if (iSubAdj(iSemId,usSemNum,sSemOper)) {
+						semundo(rpstSemSet, pstSops, i);
+						unlock(gSemGate); 
+						return;
+					}
+				}
+				change++;
 				continue;
 			}
 			/* Can't decrement. */
-			semundo(rpstSemSet->sem_base, pstSops, i);
-			if (usSemFlg & IPC_NOWAIT) {
-				/* Adjust???*/
+			semundo(rpstSemSet, pstSops, i);
+			if (sSemFlg & IPC_NOWAIT) {
 				if (u.u_error == 0)
 					u.u_error = EAGAIN;
 				unlock(gSemGate); 
 				return;
 			} else {/* Go to sleep */
-				/* Adjust???*/
 				unlock(gSemGate); 
 				if (semwait(iSemId, &rpstSem->semncnt) < 0) {
 					return;
@@ -467,18 +504,24 @@ TRY_AGAIN:	/* Repeat the semaphore set */
 		} 
 		if (sSemOper == 0) {
 			if (iSemPerm(rpstSemSet, SEM_R)) { /* cannot read */
+				/* If somebody cannot read it does not
+				 * mean that one couldn't alter.
+				 */
+				if (iSemPerm(rpstSemSet, SEM_A))
+					semundo(rpstSemSet, pstSops, i);
+				
 				unlock(gSemGate); 
 				return;
 			}
-			if (rpstSem->semval == 0) 
+			if (rpstSem->semval == 0) {
 				continue;
-
+			}
 			/* Semaphore value isn't 0. Undo all previous
 			 * operations.
 			 */
-			semundo(rpstSemSet->sem_base, pstSops, i);
+			semundo(rpstSemSet, pstSops, i);
 
-			if (usSemFlg & IPC_NOWAIT) {
+			if (sSemFlg & IPC_NOWAIT) {
 				if (u.u_error == 0)
 					u.u_error = EAGAIN;
 				unlock(gSemGate); 
@@ -496,14 +539,42 @@ TRY_AGAIN:	/* Repeat the semaphore set */
 				unlock(gSemGate); 
 				return;
 			}
+			if (sSemFlg & SEM_UNDO) {
+				if (iSubAdj(iSemId, usSemNum, sSemOper)) {
+					semundo(rpstSemSet, pstSops, i);
+					unlock(gSemGate); 
+					return;
+				}
+			}
+			if (rpstSem->semval > SEMVMX - sSemOper) {
+				semundo(rpstSemSet, pstSops, i);
+				/* semundo would not adjust current semaphore */
+				if (sSemFlg & SEM_UNDO) {
+					iSubAdj(iSemId, usSemNum, -sSemOper);
+				}
+				u.u_error = ERANGE;
+				unlock(gSemGate); 
+				return;
+			}
 			rpstSem->semval += sSemOper;
+
 			/* Wake up waiting processes */
 			if (rpstSem->semncnt)
 				wakeup(&rpstSem->semncnt);
-			/* Adjust???*/
+			change++;
+			continue;
 		}
 	}
-	rpstSemSet->sem_otime = timer.t_time;	/* adjust operation time */
+	rpstSemSet->sem_otime = timer.t_time;	/* adjust semop time */
+	if (change) /* Semaphore was changed */
+		rpstSemSet->sem_ctime = timer.t_time;
+	
+	/* Go through all set again and set pid of last semop */
+	for (i = 0, pstSemBuf = pstSops; i < uNsops; i++, pstSemBuf++) {
+		usSemNum = getusd(&(pstSemBuf->sem_num));
+		rpstSem = rpstSemSet->sem_base + usSemNum;
+		rpstSem->sempid = SELF->p_pid;
+	}
 	unlock(gSemGate);
 	return 0;				/* return last prev semval */
 }
@@ -537,25 +608,32 @@ unsigned short	*usSleepEvent;	/* Could be semcnt or semzcnt */
 }
 	
 /*
- * Undo a Semaphore Operation.
+ * Undo a Semaphore Operation. It should undo an adjust values too.
  */
-semundo(pstSem, pstSemOp, iUndo)
-struct sem	*pstSem;	/* Pointer to the first of the semaphores */
+semundo(pstSemSet, pstSemOp, iUndo)
+struct semid_ds	*pstSemSet;	/* Pointer to the semaphore set */
 struct sembuf	*pstSemOp;	/* Pointer to the undo operation */
 int		iUndo;		/* Number of semaphores to undo */
 {
 	register struct sem	*rpstSem;	/* */
 	register struct sembuf	*rpstBuf;	/* */
 	register int		i;		/* Loop index */
+	int			iSemId;		/* Semaphore id */
 	unsigned short		usSemNum;	/* Semaphore number */
 	short			sSemOper;	/* Value to undo */
+	short			sSemFlg;	/* Semaphore flag */
 
-	rpstSem = pstSem;
+	rpstSem = pstSemSet->sem_base;
 	rpstBuf = pstSemOp;
+	iSemId = pstSemSet - semids;
 	for (i = 0; i < iUndo; i++) {
 		usSemNum = getusd( &(rpstBuf->sem_num) );
 		sSemOper  = getusd( &(rpstBuf->sem_op) );
+		sSemFlg  = getusd( &(rpstBuf->sem_flg) );
 		rpstSem->semval -= sSemOper;
+		if (sSemFlg & SEM_UNDO) {
+			iSubAdj(iSemId, usSemNum, -sSemOper);
+		}
 		rpstBuf++;
 		rpstSem++;
 	}		
@@ -654,12 +732,101 @@ int	iSemNumber;	/* Requested number of the semaphores in the set */
 }
 
 /*
- * Set semaphore adjust values.
+ * Subtract iValue from the adjust value for the specified
+ * semaphore for the specified process.
  */
-void	vSemAdj(iSemId, iSemNum, iValue)
-int	iSemId;		/* Semaphore set id */
-int	iSemNum;	/* Semaphore number */
-int	iValue;		/* Adjust value */
+int iSubAdj(iSemId, usSemNum, sValue)
+int		iSemId;		/* Semaphore set id */
+unsigned short	usSemNum;	/* Semaphore number */
+short		sValue;		/* Adjust value */
 {
-/* Code should be written */
+	PROC		*pp;		/* Current process */
+	struct sem_undo	*unPrev, 	/* Previous and next pointers to */
+			*unNext;	/* sem_undo structures link list */
+	int		newAdjust;	/* New adjust value */
+
+	pp = SELF;	/* Get the current process */
+
+	/* Look if adjust value for semaphore was set */
+	for (unNext = pp->p_semu; unNext != NULL; unNext = unPrev->un_np) {
+		if (unNext->un_num == usSemNum && unNext->un_id == iSemId) {
+			newAdjust = unNext->un_aoe - sValue;
+			if (newAdjust < -SEMVMX || newAdjust > SEMVMX) {
+				u.u_error = ERANGE;
+				return -1;
+			}
+			unNext->un_aoe = newAdjust; /* Found and adjust */
+			return 0;
+		}
+		unPrev = unNext;
+	}
+	/* There is no adjust value for semaphore.
+	 * We have to allocate space and creat a new adjust value.
+	 */
+	if ((unNext = kalloc(sizeof(struct sem_undo))) == NULL) {
+		u.u_error = ENOSPC;
+		return -1;
+	}
+	/* Set values for the next entry */
+	unNext->un_np = NULL;
+	unNext->un_aoe -= sValue;
+	unNext->un_num = usSemNum;
+	unNext->un_id = iSemId;
+	
+	/* Put new entry at the end of link list */
+	if (pp->p_semu == NULL)
+		pp->p_semu = unNext;	/* New link list */
+	else				/* Add entry to the existing list */
+		unPrev->un_np = unNext;	/* unPrev is a last entry in the list */
+	return 0;
+}
+
+/*
+ * Clear adjust value for all process.
+ */
+void	vClearAdj(iSemId, usSemNum)
+int	iSemId;		/* Semaphore set id */
+int	usSemNum;	/* Semaphore number */
+{
+	PROC		*pp;		/* process */
+	struct sem_undo	*unNext; 	/* sem_undo structures */
+
+	/* Go through all processes and zero the proper undo entry */
+	for (pp = &procq; (pp = pp->p_nforw) != &procq; ) {
+		/* Look if adjust value for semaphore was set. */
+		for (unNext = pp->p_semu; unNext != NULL; 
+						unNext = unNext->un_np) {
+			if (unNext->un_num == usSemNum 
+						&& unNext->un_id == iSemId)
+				unNext->un_aoe = 0;	/* Found */
+		}
+	}		
+}
+
+/*
+ * Adjust all semaphores and remove sem_undo link list.
+ */
+semAllAdjust(pp)
+PROC	*pp;
+{
+	struct sem_undo	*unPrev, 	/* Previous and next pointers to */
+			*unNext;	/* sem_undo structures link list */
+	struct semid_ds	*pstSemSet;	/* Semaphore set */
+	struct sem	*pstSem;	/* Semaphore */
+
+	if ((unNext = pp->p_semu) == NULL)
+		return;
+
+	while (unNext != NULL) {
+		pstSemSet = semids + unNext->un_id;	/* Requested set */
+		pstSem = pstSemSet->sem_base + unNext->un_num;
+		pstSem->semval += unNext->un_aoe;
+		pstSem->sempid = SELF->p_pid;
+		pstSemSet->sem_ctime = timer.t_time;
+		unPrev = unNext;
+		unNext = unPrev->un_np;
+		kfree(unPrev);
+	}
+	/*kfree(pp->p_semu);*/
+	pp->p_semu = NULL;
 }
