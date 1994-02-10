c6f8320c3839287feba0e8a2f044348cf3f51628
Hal Snyder <hal@mwc.com>
1994-02-09 23:32:39 -0600

    *** empty log message ***

--- usr/include/stdarg.h
+++ usr/include/stdarg.h
@@ -1,16 +1,19 @@
 /* (-lgl
- * 	COHERENT Version 3.2
- * 	Copyright (c) 1982, 1993 by Mark Williams Company.
- * 	All rights reserved. May not be copied without permission.
+ *	Coherent 386 release 4.2
+ *	Copyright (c) 1982, 1993 by Mark Williams Company.
+ *	All rights reserved. May not be copied without permission.
+ *	For copying permission and licensing info, write licensing@mwc.com
  -lgl) */
-/*
- * stdarg.h
- * C variable arguments header.
- * Draft Proposed ANSI C Standard, Section 4.8, 12/7/88 draft.
- */
 
 #ifndef	__STDARG_H__
 #define	__STDARG_H__
+
+/*
+ * Please note that passing a type to va_arg() that would be widened when
+ * promoted to integer type causes undefined behavior. This includes char,
+ * short and enumeration types (since many translators use the narrowest
+ * possible width for storing an enumeration).
+ */
 
 #include <common/ccompat.h>
 #include <common/__valist.h>
@@ -18,31 +21,26 @@
 typedef	__va_list		va_list;
 
 /*
- * On true ANSI/ISO C systems, we just use '...' to get the base address of
- * the argument space. In either system, we have several options as to how
- * we attempt to work around alignment issues (if we do at all).
- *
  * As a convenience, we round sizes up to a minimum size of "int". This may
  * help people who are confused about what the implementation is permitted
- * to do with enumeration types, for instance. This setup below will break
- * with systems that put the padding below the datum.
+ * to do with enumeration types, for instance. Note that this does introduce
+ * some dependence on the endianness of the machine, however, because those
+ * machines which are big-endian will have put padding below the datum.
  */
 
 #define	__size_to_int(type)	(sizeof (type) < sizeof (int) ? \
 				 sizeof (int) : sizeof (type))
-
-#if	__cplusplus
-
-#define	va_start(ap, pN)	(ap = ...)
-
+#if	_I386
+# define	__size_to_datum(type)	__size_to_int (type)
 #else
+# define	__size_to_datum(type)	sizeof (type)
+#endif
+
 
 #define	va_start(ap, pN)	(ap = (va_list) \
 					((char *) & pN + __size_to_int (pN)))
-#endif
-
 #define	va_arg(ap, type)	\
-	((* (type *) ((ap += __size_to_int (type)) - __size_to_int (type))))
+	((* (type *) ((ap += __size_to_int (type)) - __size_to_datum (type))))
 #define	va_end(ap)		((void) 0)
 
 #endif	/* ! defined (__STDARG_H__) */
